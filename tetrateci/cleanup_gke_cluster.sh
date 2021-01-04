35ad50819f044097f2677861ddf2c026d12af2ae
Kuntal Majumder <kuntal@infracloud.io>
2021-01-04 13:23:42 +0530

    Use the short sha for gke cluster names

--- tetrateci/cleanup_gke_cluster.sh
+++ tetrateci/cleanup_gke_cluster.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CLUSTER_NAME="test-istio-$GITHUB_SHA"
+SHA8=$(git rev-parse --short $GITHUB_SHA)
+CLUSTER_NAME="test-istio-$SHA8"
 gcloud container clusters delete $CLUSTER_NAME --region us-central1-c --quiet
--- tetrateci/create_gke_cluster.sh
+++ tetrateci/create_gke_cluster.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-CLUSTER_NAME="test-istio-$GITHUB_SHA"
-gcloud container clusters create $CLUSTER_NAME --machine-type "e2-standard-2" --num-nodes 3 --region=us-central1-c
+SHA8=$(git rev-parse --short $GITHUB_SHA)
+CLUSTER_NAME="test-istio-$SHA8"
+gcloud container clusters create $CLUSTER_NAME --machine-type "e2-standard-2" --num-nodes 3 --region=us-central1-c --enable-network-policy
 gcloud config set container/use_client_certificate False
 gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1-c
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value core/account)"
-gcloud container clusters update $CLUSTER_NAME --update-addons=NetworkPolicy=ENABLED --region us-central1-c
-gcloud container clusters update $CLUSTER_NAME --enable-network-policy --region us-central1-c
