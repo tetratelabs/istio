10e2336f55ff645884c615340d4a7c04d275876e
Kuntal Majumder <kuntal@infracloud.io>
2021-01-05 10:12:00 +0530

    test release builder

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir /tmp/istio-release
+
+sudo gem install fpm
+sudo apt install gettext_base -y
+envsubst < ./tetrateci/manifest.yaml.in > manifest.yaml
 git clone https://github.com/istio/release-builder --depth=1
 cd release-builder
-go run main.go build --manifest ../ci/manifest.yaml
+go run main.go build --manifest ../manifest.yaml
+go run main.go publish --manifest ../manifest.yaml --release ${TAG}
