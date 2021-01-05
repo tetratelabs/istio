63b09e7581c35edb47d881664267524040f6c069
Kuntal Majumder <kuntal@infracloud.io>
2021-01-05 18:12:59 +0530

    publish the archives to bintray

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -9,3 +9,13 @@ git clone https://github.com/istio/release-builder --depth=1
 cd release-builder
 go run main.go build --manifest ../manifest.yaml
 go run main.go publish --release /tmp/istio-release/out --dockerhub hellozee-docker-istio-temp.bintray.io
+
+PACKAGES=$(ls /tmp/istio-release/out/ | grep "istio*")
+for package in $PACKAGES; do
+    NAME=$(cut -d '-' -f 1 <<< $package)
+    echo "Publishing $package"
+    curl -T /tmp/istio-release/out/$package -u$BINTRY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/$NAME/$TAG/$package
+done
+
+curl -X POST -u$BINTRAY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/istio/$TAG/publish 
+curl -X POST -u$BINTRAY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/istioctl/$TAG/publish 
