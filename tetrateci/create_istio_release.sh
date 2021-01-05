88332a24de7b935b572767993cfbb1f4d2091589
Kuntal Majumder <kuntal@infracloud.io>
2021-01-05 10:56:48 +0530

    use the correct flags for publishing

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -3,9 +3,9 @@
 mkdir /tmp/istio-release
 
 sudo gem install fpm
-sudo apt-get install gettext-base go-bindata -y
+sudo apt-get install go-bindata -y
 envsubst < ./tetrateci/manifest.yaml.in > manifest.yaml
 git clone https://github.com/istio/release-builder --depth=1
 cd release-builder
 go run main.go build --manifest ../manifest.yaml
-go run main.go publish --manifest ../manifest.yaml --release ${TAG}
+go run main.go publish --release /tmp/istio-release/out
