8d2b7008de3a87a6e1b18837d527f69a1dc00ae8
Kuntal Majumder <kuntal@infracloud.io>
2021-01-07 23:35:50 +0530

    changes for workflows needs to be checked under head

--- tetrateci/fetch_all_tags.sh
+++ tetrateci/fetch_all_tags.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-UPSTREAM_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/istio/istio.git"
-git remote add upstream $UPSTREAM_REPO
+
+git remote add upstream https://github.com/istio/istio
+git fetch --tags origin
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f1
 git fetch --tags upstream
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f2
+
+echo "print f1"
+cat f1
+echo "print f2"
+cat f2
+
+tags=$(comm -13 f1 f2)
+rm f1 f2
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+for tag in $tags; do
+    branch=$( echo $tag | rev | cut -d. -f2- | rev )
+    git checkout -b tetrate-release-$branch origin/tetrate-release-$branch
+    git merge $tag --no-edit
+    git tag tetrate-test-$tag
+    git push origin tetrate-release-$branch --tags
+done
+
 git push --tags origin
