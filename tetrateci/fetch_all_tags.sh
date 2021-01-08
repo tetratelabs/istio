d92ec3a6344994754f6b3448ca74c5cd3692db14
Kuntal Majumder <kuntal@infracloud.io>
2021-01-08 20:56:22 +0530

    create branch if not there and add docs

--- tetrateci/fetch_all_tags.sh
+++ tetrateci/fetch_all_tags.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
+# we first fetch all the tags from origin and then from upstream
+# the tag should be in form of number.number.number
 git remote add upstream https://github.com/istio/istio
 git fetch --tags origin
-git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f1
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > /tmp/oldtags
 git fetch --tags upstream
-git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f2
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > /tmp/newtags
 
-echo "print f1"
-cat f1
-echo "print f2"
-cat f2
+echo "print oldtags"
+cat /tmp/oldtags
+echo "print newtags"
+cat /tmp/newtags
 
-tags=$(comm -13 f1 f2)
-rm f1 f2
+# then compare the list and pick the ones unique to the second list
+tags=$(comm -13 /tmp/oldtags /tmp/newtags)
 
 git config user.name github-actions
 git config user.email github-actions@github.com
 
 for tag in $tags; do
+    # the branch names are suffixed with the first 2 numbers in the version
     branch=$( echo $tag | rev | cut -d. -f2- | rev )
-    git checkout -b tetrate-release-$branch origin/tetrate-release-$branch
+    if [[ ! $(git rev-parse --verify --quiet origin/tetrate-release-$branch) ]]; then
+        # create the tetrate release branch if it doesn't exist with the workflows
+        git checkout tetrate-workflow
+        git checkout -b tetrate-release-$branch 
+    else
+        git checkout -b tetrate-release-$branch origin/tetrate-release-$branch
+    fi
     git merge $tag --no-edit
     git tag tetrate-test-$tag
     git push origin tetrate-release-$branch --tags
 done
 
+# finally push all the tags
 git push --tags origin
