53695db5ccea7371924441da8da5434a844195ec
Kuntal Majumder <kuntal@infracloud.io>
2021-01-11 20:53:18 +0530

    moved hardcoded stuff into variables

--- tetrateci/README.md
+++ tetrateci/README.md
@@ -7,8 +7,12 @@
 4. AZURE_CREDENTIALS : [Note](https://github.com/Azure/login#configure-deployment-credentials)
 5. BINTRAY_API_KEY
 6. BINTRAY_USER
-7. GCP_PROJECT_ID
-8. GCP_SA_KEY
-9. PAT :  [ Note : Personal access token of a github account who can push to the repo ]
-
-Need to replace all the references to Bintray and the Azure resource group, marked with `change`.
+7. DEPLOY_HUB : [ Note : Link to the dockerr registry where final images would be pushed ]
+8. GCP_PROJECT_ID
+9. GCP_SA_KEY
+10. PAT :  [ Note : Personal access token of a github account who can push to the repo ]
+11. TEST_HUB : [ Note : Link to docker registry for pushing test images ] 
+12. TEST_HUB_USER
+13. TEST_HUB_PASS
+14. BINTRAY_ARCHIVE_API : [ Note : The archive API url, example - https://api.bintray.com/content/hellozee/istio-archives ]
+15. AZURE_RESOURCE : [ Note : The azure resource name corresponding to the creds ]
--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -8,15 +8,15 @@ export IMAGE_VERSION=$(curl https://raw.githubusercontent.com/istio/test-infra/m
 make shell
 mkdir /tmp/istio-release
 go run main.go build --manifest manifest.yaml
-go run main.go validate --release /tmp/istio-release/out
+# go run main.go validate --release /tmp/istio-release/out # seems like it fails if not all the targets are generated
 go run main.go publish --release /tmp/istio-release/out --dockerhub $HUB
 
 PACKAGES=$(ls /tmp/istio-release/out/ | grep "istio*")
 for package in $PACKAGES; do
     NAME=$(cut -d '-' -f 1 <<< $package)
     echo "Publishing $package"
-    curl -T /tmp/istio-release/out/$package -u$BINTRAY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/$NAME/$TAG/$package #change
+    curl -T /tmp/istio-release/out/$package -u$BINTRAY_USER:$API_KEY $BINTRAY_API/$NAME/$TAG/$package 
 done
 
-curl -X POST -u$BINTRAY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/istio/$TAG/publish  #change
-curl -X POST -u$BINTRAY_USER:$API_KEY https://api.bintray.com/content/hellozee/istio-archives/istioctl/$TAG/publish  #change
+curl -X POST -u$BINTRAY_USER:$API_KEY $BINTRAY_API/istio/$TAG/publish
+curl -X POST -u$BINTRAY_USER:$API_KEY $BINTRAY_API/istioctl/$TAG/publish
