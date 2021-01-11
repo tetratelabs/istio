1d233085c0535dd27a329c9137a31ff50b1e135e
Kuntal Majumder <kuntal@infracloud.io>
2021-01-11 17:28:45 +0530

    use matrix strategy to spin up clusters of different versions

--- tetrateci/create_aks_cluster.sh
+++ tetrateci/create_aks_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SHA8=$(git rev-parse --short $GITHUB_SHA)
 CLUSTER_NAME="test-istio-$SHA8"
-az aks create --resource-group getistio --name $CLUSTER_NAME --node-count 2 --generate-ssh-keys # change the resource group
+az aks create --resource-group getistio --name $CLUSTER_NAME --node-count 2 --generate-ssh-keys --kubernetes-version $VER # change the resource group
 az aks get-credentials --resource-group getistio --name $CLUSTER_NAME # change the resource group
--- tetrateci/create_gke_cluster.sh
+++ tetrateci/create_gke_cluster.sh
@@ -2,7 +2,7 @@
 
 SHA8=$(git rev-parse --short $GITHUB_SHA)
 CLUSTER_NAME="test-istio-$SHA8"
-gcloud container clusters create $CLUSTER_NAME --machine-type "e2-standard-2" --num-nodes 3 --region=us-central1-c --enable-network-policy
+gcloud container clusters create $CLUSTER_NAME --machine-type "e2-standard-2" --num-nodes 3 --region=us-central1-c --enable-network-policy --cluster-version $VER --release-channel "regular"
 gcloud config set container/use_client_certificate False
 gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1-c
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value core/account)"
