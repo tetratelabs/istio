45c6d031d8782b7b450ad896786cba019c14b1d2
Kuntal Majumder <kuntal@infracloud.io>
2021-01-11 13:05:20 +0530

    validate istio releases before making one

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -3,10 +3,11 @@
 mkdir /tmp/istio-release
 sudo gem install fpm
 sudo apt-get install go-bindata -y
-envsubst < ./tetrateci/manifest.yaml.in > manifest.yaml
 git clone https://github.com/istio/release-builder --depth=1
+envsubst < ./tetrateci/manifest.yaml.in > ./release-builder/manifest.yaml
 cd release-builder
-go run main.go build --manifest ../manifest.yaml
+go run main.go build --manifest manifest.yaml
+go run main.go validate --release /tmp/istio-release/out
 go run main.go publish --release /tmp/istio-release/out --dockerhub $HUB
 
 PACKAGES=$(ls /tmp/istio-release/out/ | grep "istio*")
