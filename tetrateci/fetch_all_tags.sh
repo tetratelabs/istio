b032454e7e7d8c85fc57d3eafb0c55fd00fafdd2
Kuntal Majumder <kuntal@infracloud.io>
2021-01-12 20:55:29 +0530

    parent ad3ff052f084361fa9c815b0041973ba6cbe14e2 author Kuntal Majumder <kuntal@infracloud.io> 1610465129 +0530 committer Devarajan Ramaswamy <deva.ramaswamy@tetrate.io> 1610627757 +0530
    parse tag and set as part of the workflow

--- tetrateci/fetch_all_tags.sh
+++ tetrateci/fetch_all_tags.sh
@@ -16,6 +16,9 @@ cat /tmp/newtags
 
 # then compare the list and pick the ones unique to the second list
 tags=$(comm -13 /tmp/oldtags /tmp/newtags)
+echo "==================="
+echo "New issue tags to be created $tags"
+echo "==================="
 
 git config user.name github-actions
 git config user.email github-actions@github.com
@@ -25,12 +28,11 @@ for tag in $tags; do
     branch=$( echo $tag | cut -d. -f1,2 )
     if [[ ! $(git rev-parse --verify --quiet origin/tetrate-release-$branch) ]]; then
         # create the tetrate release branch if it doesn't exist with the workflows
-        git checkout tetrate-workflow
-        git checkout -b tetrate-release-$branch 
+        git checkout -b tetrate-release-$branch origin/tetrate-workflow
     else
         git checkout -b tetrate-release-$branch origin/tetrate-release-$branch
     fi
-    git merge $tag --no-edit
+    git merge $tag --no-edit --allow-unrelated-histories
     git tag test-$tag-tetrate-v0
     git push origin tetrate-release-$branch --tags
 done
