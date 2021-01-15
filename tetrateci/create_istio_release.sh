d9bd6fd8b216c5bbba4632c5148a2c0115688014
Kuntal Majumder <kuntal@infracloud.io>
2021-01-15 11:29:59 +0530

    manually download and tag the images to be sure

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -2,6 +2,7 @@
 
 # HACK : the github runner runs out of space sometimes so removing the 21 GB dotnet folder
 # Temporary thing, we should be moving to a custom runner instead.
+echo "Trying to delete that massive dotnet folder"
 [ -d "/usr/share/dotnet" ] && sudo rm -rf /usr/share/dotnet
 
 sudo gem install fpm
@@ -11,8 +12,36 @@ git clone https://github.com/istio/release-builder --depth=1
 envsubst < ./istio/tetrateci/manifest.yaml.in > ./release-builder/manifest.yaml
 cd release-builder
 cp -r ../istio .
-export IMAGE_VERSION=$(curl https://raw.githubusercontent.com/istio/test-infra/master/prow/config/jobs/release-builder.yaml | grep "image: gcr.io" | head -n 1 | cut -d: -f3)
+#export IMAGE_VERSION=$(curl https://raw.githubusercontent.com/istio/test-infra/master/prow/config/jobs/release-builder.yaml | grep "image: gcr.io" | head -n 1 | cut -d: -f3)
 # make shell TODO: https://github.com/tetratelabs/getistio/issues/82
+
+ORIGINAL_TAG=$(cut -d"-" -f1 <<< $TAG)
+echo "Original Tag $ORIGINAL_TAG"
+
+echo "Downloading https://github.com/istio/api"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/api istio-api
+cd istio-api && git tag $TAG && cd ..
+
+echo "Downloading https://github.com/istio/pkg"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/pkg istio-pkg
+cd istio-pkg && git tag $TAG && cd ..
+
+echo "Downloading https://github.com/istio/client-go"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/client-go istio-client-go
+cd istio-client-go && git tag $TAG && cd ..
+
+echo "Downloading https://github.com/istio/gogo-genproto"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/gogo-genproto istio-gogo-genproto
+cd istio-gogo-genproto && git tag $TAG && cd ..
+
+echo "Downloading https://github.com/istio/test-infra"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/test-infra istio-test-infra
+cd istio-test-infra && git tag $TAG && cd ..
+
+echo "Downloading https://github.com/istio/tools"
+git clone --depth 1 --branch $ORIGINAL_TAG https://github.com/istio/tools istio-tools
+cd istio-tools && git tag $TAG && cd ..
+
 mkdir /tmp/istio-release
 go run main.go build --manifest manifest.yaml
 # go run main.go validate --release /tmp/istio-release/out # seems like it fails if not all the targets are generated
