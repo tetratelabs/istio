5fa7c398b8a2b4ac03a83b3666c6114ab6201b1c
Sergi Castro <sergikstro@gmail.com>
2021-01-23 12:28:32 +0100

    set AKS minimal image size to let the test run

--- getistioci/create_aks_cluster.sh
+++ getistioci/create_aks_cluster.sh
@@ -4,5 +4,5 @@ set -o pipefail
 SHA8=$(git rev-parse --short $GITHUB_SHA)
 SUFFIX=$(sed 's/\.//g' <<< $VER)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
-az aks create --resource-group $RESOURCE --name $CLUSTER_NAME --node-count 2 --generate-ssh-keys --kubernetes-version $VER
+az aks create --resource-group $RESOURCE --name $CLUSTER_NAME --node-count 2 --generate-ssh-keys --kubernetes-version $VER -s standard_d8s_v3
 az aks get-credentials --resource-group $RESOURCE --name $CLUSTER_NAME
