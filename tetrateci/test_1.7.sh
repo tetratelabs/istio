4806b4d0d19b8682f1ecd93f8dba197c06cd69b4
Sergi Castro <sergikstro@gmail.com>
2021-01-24 12:46:09 +0100

    Separate test into versions as first step (#20)
    * Separate test into versions as first step
    
    As version increases new tests are added so, we cannot run the same
    tests for all the versions.
    This way we can control in different files the tests that are needed for
    each minor version.
    
    * Add gke 1.16 and 1.18 versions in e2e
    
    * Run test depending on distro
    
    * Replace MINORVER with REL_BRANCH_VER in e2e workflow
    
    * Rename tetrate to getistio
    
    * use -count=1 in tests
    
    * Will rename branch tetrate-workflow
    
    * addressing comments
    
    * set AKS minimal image size to let the test run
    
    * change 1.17 aks version
    
    * Revert "Rename tetrate to getistio"
    
    This reverts commit 0a09507c4a161cf942b78b396724ec0ca944a818.
    
    * Revert "Will rename branch tetrate-workflow"
    
    This reverts commit 91374a8f0709fd04667daae617517d1bdc3198ec.

---
+++ tetrateci/test_1.7.sh
@@ -0,0 +1,29 @@
+#!env bash
+set -e
+
+if [[ ${CLUSTER} == "gke" ]]; then
+  # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
+  pip install pyyaml --user && ./tetrateci/gen_iop.py
+  CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
+fi
+
+go test -count=1 -tags=integ ./tests/integration/operator/...  -p 1  -test.v
+go test -count=1 -tags=integ -timeout 30m -run='TestEmptyCluster|TestFileOnly|TestDirectoryWithoutRecursion|TestDirectoryWithRecursion|TestInvalidFileError|TestJsonInputFile|TestJsonOutput|TestKubeOnly|TestFileAndKubeCombined|TestAllNamespaces|TestTimeout|TestErrorLine|TestWait|TestVersion|TestDescribe|TestAddToAndRemoveFromMesh|TestProxyConfig|TestProxyStatus|TestAuthZCheck|TestLocality|TestMain|TestMirroring|TestMirroringExternalService|TestTproxy|TestValidation|TestEnsureNoMissingCRDs|TestWebhook' ./tests/integration/pilot/ -p 1 -test.v 
+go test -count=1 -tags=integ ./tests/integration/pilot/analysis/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/pilot/revisions/... -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m -run='TestStatsFilter|TestStatsTCPFilter|TestSetup|TestIstioctlMetrics|TestTcpMetric|TestStatsFilter|TestWASMTcpMetric|TestWasmStatsFilter|TestMain|TestCustomizeMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/ca_custom_root/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/filebased_tls_origination/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/mtls_first_party_jwt/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/mtlsk8sca/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/sds_egress/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/sds_tls_origination/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/webhook/... -p 1 -test.v
+
+if [[ ${CLUSTER} == "eks" ]]; then
+  go test -count=1 -tags=integ ./tests/integration/security/chiron/... -p 1 -test.v
+fi
+
+if [[ $CLUSTER != "aks" ]]; then
+  go test -count=1 -tags=integ ./tests/integration/pilot/cni/... ${CLUSTERFLAGS} -p 1 -test.v
+fi
