2850014ae81c6b3e3ee636ae2587f2d81a37f6f5
Kuntal Majumder <12135951+hellozee@users.noreply.github.com>
2021-01-27 20:14:07 +0530

    add action to backport patches (#29)

---
+++ tetrateci/port_patches_to_release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Undefined GITHUB_TOKEN environment variable."
+  exit 1
+fi
+
+echo "Configuring git"
+
+cat <<- EOF > $HOME/.netrc
+    machine github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
+    machine api.github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
+EOF
+chmod 600 $HOME/.netrc
+
+git config user.name $GITHUB_ACTOR
+git config user.email github-actions@github.com
+
+echo "Fetching target branches"
+
+TARGETS=$(git branch -r| grep origin/tetrate-release | xargs)
+
+echo "Creating PRs"
+
+for branch in $TARGETS; do
+    echo "Getting branch name for $branch"
+    branch_name=$(cut -f2 -d"/" <<< $branch)
+    echo "Creating PR for $branch_name"
+    hub pull-request -b $branch_name -m "AUTO: Backporting patches to $branch_name"
+done
