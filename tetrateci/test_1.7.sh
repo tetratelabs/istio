e86aa3312b76564b2a5cfcf900e0ffaaefa4c3df
Kuntal Majumder <kuntal@infracloud.io>
2021-01-28 14:11:37 +0530

    patch for missing ip

--- tetrateci/test_1.7.sh
+++ tetrateci/test_1.7.sh
@@ -26,23 +26,23 @@ go test -count=1 -tags=integ ./tests/integration/security/sds_tls_origination/..
 go test -count=1 -tags=integ ./tests/integration/security/webhook/... -p 1 -test.v
 
 if [[ ${CLUSTER} == "eks" ]]; then
-  go test -count=1 -tags=integ -timeout 30m -run='TestWait|TestVersion|TestDescribe|TestAddToAndRemoveFromMesh|TestProxyConfig|TestProxyStatus|TestAuthZCheck|TestMain|TestMirroring|TestMirroringExternalSerivce|TestTraffic' ./tests/integration/pilot/ -p 1 -test.v 
-  go test -count=1 -tags=integ -run 'TestAuthorization_mTLS|TestAuthorization_JWT|TestAuthorization_WorkloadSelector|TestAuthorization_Deny|TestAuthorization_NegativeMatch|TestAuthorization_EgressGateway|TestAuthorization_TCP|TestAuthorization_Conditions|TestAuthorization_GRPC|TestAuthorization_Path|TestRequestAuthentication|TestMain|TestMtlsHealthCheck|TestPassThroughFilterChain' ./tests/integration/security/.  -p 1 -test.v
+  git apply tetrateci/eks-ingress.patch   
   go test -count=1 -tags=integ -timeout 30m -run='TestStatsFilter|TestSetup|TestIstioctlMetrics|TestStatsFilter|TestWASMTcpMetric|TestWasmStatsFilter|TestMain|TestCustomizeMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
 else
   go test -count=1 -tags=integ ./tests/integration/mixer/. -p 1 -test.v
   go test -count=1 -tags=integ ./tests/integration/mixer/envoy/...  -p 1 -test.v
   go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/policy/. -p 1 -test.v
   go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/telemetry/... -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/. -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/vm/. -run='TestTrafficShifting|TestVmOS' -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/ingress/. -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/security/.  -p 1 -test.v
-  go test -count=1 -tags=integ ./tests/integration/security/sds_ingress/.  -p 1 -test.v
-  go test -count=1 -tags=integ ./tests/integration/security/sds_ingress_k8sca/.  -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/telemetry/. -p 1 -test.v
   go test -count=1 -tags=integ -timeout 30m ./tests/integration/telemetry/stats/... -p 1 -test.v
+  go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/ingress/. -p 1 -test.v
 fi
+
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/. -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/vm/. -run='TestTrafficShifting|TestVmOS' -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/security/.  -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/sds_ingress/.  -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/security/sds_ingress_k8sca/.  -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/telemetry/. -p 1 -test.v
 
 
 if [[ $CLUSTER != "aks" ]]; then
