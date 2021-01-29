36895ef6187339aa60fcb6d9531ea45a0d38053b
Kuntal Majumder <kuntal@infracloud.io>
2021-01-29 18:19:45 +0530

    finish out the tests for eks 1.7

--- tetrateci/test_1.7.sh
+++ tetrateci/test_1.7.sh
@@ -8,6 +8,10 @@ if [[ ${CLUSTER} == "gke" ]]; then
   git apply tetrateci/chiron-gke.patch
 fi
 
+if [[ ${CLUSTER} == "eks" ]]; then
+  git apply tetrateci/eks-ingress.1.7.patch
+fi
+
 go test -count=1 -tags=integ ./tests/integration/operator/...  -p 1  -test.v
 go test -count=1 -tags=integ ./tests/integration/galley/...  -p 1  -test.v
 go test -count=1 -tags=integ ./tests/integration/pilot/analysis/... -p 1 -test.v
@@ -25,25 +29,20 @@ go test -count=1 -tags=integ ./tests/integration/security/sds_egress/... -p 1 -t
 go test -count=1 -tags=integ ./tests/integration/security/sds_tls_origination/... -p 1 -test.v
 go test -count=1 -tags=integ ./tests/integration/security/webhook/... -p 1 -test.v
 
-if [[ ${CLUSTER} == "eks" ]]; then
-  git apply tetrateci/eks-ingress.1.7.patch   
-  go test -count=1 -tags=integ -timeout 30m -run='TestStatsFilter|TestSetup|TestIstioctlMetrics|TestStatsFilter|TestWASMTcpMetric|TestWasmStatsFilter|TestMain|TestCustomizeMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
-else
-  go test -count=1 -tags=integ ./tests/integration/mixer/. -p 1 -test.v
-  go test -count=1 -tags=integ ./tests/integration/mixer/envoy/...  -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/policy/. -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/telemetry/... -p 1 -test.v
-  go test -count=1 -tags=integ -timeout 30m ./tests/integration/telemetry/stats/... -p 1 -test.v
-fi
-
+# Note: if this compains about unknown field namespaceSelector, clean the x-k8s crds
 go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/ingress/. -p 1 -test.v
+
 go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/. -p 1 -test.v
 go test -count=1 -tags=integ -timeout 30m ./tests/integration/pilot/vm/. -p 1 -test.v
 go test -count=1 -tags=integ -timeout 30m ./tests/integration/security/.  -p 1 -test.v
 go test -count=1 -tags=integ ./tests/integration/security/sds_ingress/.  -p 1 -test.v
 go test -count=1 -tags=integ ./tests/integration/security/sds_ingress_k8sca/.  -p 1 -test.v
 go test -count=1 -tags=integ -timeout 30m ./tests/integration/telemetry/. -p 1 -test.v
-
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/. -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/mixer/envoy/...  -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/policy/. -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m ./tests/integration/mixer/telemetry/... -p 1 -test.v
+go test -count=1 -tags=integ -timeout 30m -run='TestStatsFilter|TestStatsFilter|TestSetup|TestWasmStatsFilter|TestTcpMetric|TestIstioctlMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
 
 if [[ $CLUSTER != "aks" ]]; then
   go test -count=1 -tags=integ ./tests/integration/pilot/cni/... ${CLUSTERFLAGS} -p 1 -test.v
