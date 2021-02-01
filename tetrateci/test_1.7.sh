9c53cf8ea2378aaaa8e1f2a359b005266d23f598
Kuntal Majumder <kuntal@infracloud.io>
2021-02-01 14:12:15 +0530

    finalized the tests being in 1.7

--- tetrateci/test_1.7.sh
+++ tetrateci/test_1.7.sh
@@ -13,12 +13,28 @@ if [[ ${CLUSTER} == "eks" ]]; then
 fi
 
 go test -count=1 ./tests/integration/operator/...  -p 1  -test.v
+
 go test -count=1 ./tests/integration/galley/...  -p 1  -test.v
+
 go test -count=1 ./tests/integration/pilot/analysis/... -p 1 -test.v
 go test -count=1 ./tests/integration/pilot/locality/... -p 1 -test.v
 go test -count=1 ./tests/integration/pilot/revisions/... -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/pilot/. -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/pilot/vm/. -p 1 -test.v
+# Note: if this compains about unknown field namespaceSelector, clean the x-k8s crds
+go test -count=1 -timeout 30m ./tests/integration/pilot/ingress/. -p 1 -test.v
+
 go test -count=1 ./tests/integration/mixer/outboundtrafficpolicy  -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/mixer/. -p 1 -test.v
+go test -count=1 ./tests/integration/mixer/envoy/...  -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/mixer/policy/. -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/mixer/telemetry/... -p 1 -test.v
+
 go test -count=1 ./tests/integration/telemetry/outboundtrafficpolicy -p 1 -test.v
+go test -count=1 -timeout 30m ./tests/integration/telemetry/. -p 1 -test.v
+go test -count=1 -timeout 30m -run='TestStatsFilter|TestStatsFilter|TestSetup|TestWasmStatsFilter|TestTcpMetric|TestIstioctlMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
+go test -count=1 -tags=integ ./tests/integration/telemetry/tracing/...  -p 1 -test.v
+
 go test -count=1 ./tests/integration/security/ca_custom_root/... -p 1 -test.v
 go test -count=1 ./tests/integration/security/cert_provision_prometheus/... -p 1 -test.v
 go test -count=1 ./tests/integration/security/chiron/... -p 1 -test.v
@@ -28,21 +44,9 @@ go test -count=1 ./tests/integration/security/mtlsk8sca/... -p 1 -test.v
 go test -count=1 ./tests/integration/security/sds_egress/... -p 1 -test.v
 go test -count=1 ./tests/integration/security/sds_tls_origination/... -p 1 -test.v
 go test -count=1 ./tests/integration/security/webhook/... -p 1 -test.v
-
-# Note: if this compains about unknown field namespaceSelector, clean the x-k8s crds
-go test -count=1 -timeout 30m ./tests/integration/pilot/ingress/. -p 1 -test.v
-
-go test -count=1 -timeout 30m ./tests/integration/pilot/. -p 1 -test.v
-go test -count=1 -timeout 30m ./tests/integration/pilot/vm/. -p 1 -test.v
 go test -count=1 -timeout 30m ./tests/integration/security/.  -p 1 -test.v
 go test -count=1 ./tests/integration/security/sds_ingress/.  -p 1 -test.v
 go test -count=1 ./tests/integration/security/sds_ingress_k8sca/.  -p 1 -test.v
-go test -count=1 -timeout 30m ./tests/integration/telemetry/. -p 1 -test.v
-go test -count=1 -timeout 30m ./tests/integration/mixer/. -p 1 -test.v
-go test -count=1 ./tests/integration/mixer/envoy/...  -p 1 -test.v
-go test -count=1 -timeout 30m ./tests/integration/mixer/policy/. -p 1 -test.v
-go test -count=1 -timeout 30m ./tests/integration/mixer/telemetry/... -p 1 -test.v
-go test -count=1 -timeout 30m -run='TestStatsFilter|TestStatsFilter|TestSetup|TestWasmStatsFilter|TestTcpMetric|TestIstioctlMetrics' ./tests/integration/telemetry/stats/... -p 1 -test.v
 
 if [[ $CLUSTER != "aks" ]]; then
   go test -count=1 ./tests/integration/pilot/cni/... ${CLUSTERFLAGS} -p 1 -test.v
