4cc080a49d8fe1a4a7c0ff8edef229f074145350
Kuntal Majumder <12135951+hellozee@users.noreply.github.com>
2021-02-15 13:31:35 +0530

    correct the patch path (#58)

--- tetrateci/test_1.8.sh
+++ tetrateci/test_1.8.sh
@@ -4,11 +4,11 @@ set -e
 # need this variable to run the tests outside GOPATH
 export REPO_ROOT=$(pwd)
 
-git apply tetrateci/common/disable-dashboard.1.8.patch
-git apply tetrateci/common/disable-multicluster.1.8.patch
-git apply tetrateci/common/disable-ratelimiting.1.8.patch
-git apply tetrateci/common/disable-vmospost.1.8.patch
-git apply tetrateci/common/disable-stackdriver.1.8.patch
+git apply tetrateci/patches/common/disable-dashboard.1.8.patch
+git apply tetrateci/patches/common/disable-multicluster.1.8.patch
+git apply tetrateci/patches/common/disable-ratelimiting.1.8.patch
+git apply tetrateci/patches/common/disable-vmospost.1.8.patch
+git apply tetrateci/patches/common/disable-stackdriver.1.8.patch
 
 if $(grep -q "1.17" <<< ${VERSION} ); then
   git apply tetrateci/common/disable-endpointslice.1.8.patch
@@ -18,15 +18,15 @@ if [[ ${CLUSTER} == "gke" ]]; then
   # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
   pip install pyyaml --user && ./tetrateci/gen_iop.py
   CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
-  git apply tetrateci/chiron-gke.patch
+  git apply tetrateci/patches/gke/chiron-gke.patch
 fi
 
 if [[ ${CLUSTER} == "eks" ]]; then
-  git apply tetrateci/eks/eks-ingress.1.8.patch
+  git apply tetrateci/patches/eks/eks-ingress.1.8.patch
 fi
 
 if [[ ${CLUSTER} == "aks" ]]; then
-  git apply tetrateci/aks/aks-pilot.1.8.patch
+  git apply tetrateci/patches/aks/aks-pilot.1.8.patch
 fi
 
 if $(go version | grep "1.15"); then
