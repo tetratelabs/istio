c07f6c385f521b48da4394a1004146072f9112ab
Kuntal Majumder <kuntal@infracloud.io>
2021-02-15 17:27:17 +0530

    removing fips comment from normal go

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -1,7 +1,7 @@
 #!env bash
 set -e
 
-echo "Fetching FIPS compliant Go"
+echo "Fetching Go $GOLANG_VERSION"
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
 
 wget -O go.tgz "$url"
