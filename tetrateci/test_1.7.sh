d7ffbeb0aa906d51736bec73e37f89064fd6c8c8
Kuntal Majumder <kuntal@infracloud.io>
2021-02-22 17:01:51 +0530

    added better echo statements while testing

--- tetrateci/test_1.7.sh
+++ tetrateci/test_1.7.sh
@@ -29,7 +29,7 @@ for package in $PACKAGES; do
   n=0
   until [ "$n" -ge 3 ]
   do
-    echo "========================================================TRY $n========================================================"
+    echo "========================================================TESTING $package | TRY $n========================================================"
     go test -count=1 -p 1 -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
     for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
     n=$((n+1))
