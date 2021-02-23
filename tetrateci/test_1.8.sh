a6784a6aca8609fb6b788cdfd710c68b3702e7eb
Devarajan Ramaswamy <deva.ramaswamy@tetrate.io>
2021-02-24 00:22:23 +0530

    imagepull policy

--- tetrateci/test_1.8.sh
+++ tetrateci/test_1.8.sh
@@ -44,7 +44,7 @@ for package in $PACKAGES; do
   until [ "$n" -ge 3 ]
   do
     echo "========================================================TESTING $package | TRY $n========================================================"
-    go test -p 1 -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
+    go test -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
     for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
     n=$((n+1))
   done
