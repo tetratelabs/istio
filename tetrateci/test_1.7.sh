f095d7106c154bbe129f764cb24b4bd0b48caa82
Kuntal Majumder <12135951+hellozee@users.noreply.github.com>
2021-03-02 00:40:47 +0530

    Fix the workflow failures for 1.7.8 (#160)

--- tetrateci/test_1.7.sh
+++ tetrateci/test_1.7.sh
@@ -18,7 +18,7 @@ if [[ ${CLUSTER} == "gke" ]]; then
 
   echo "Applying GKE specific patches...."
   git apply tetrateci/patches/gke/chiron-gke.patch
-  git apply tetrateci/patches/gke/disable-vmospost-gke.1.7,patch
+  git apply tetrateci/patches/gke/disable-vmospost-gke.1.7.patch
 fi
 
 if [[ ${CLUSTER} == "eks" ]]; then
@@ -26,16 +26,18 @@ if [[ ${CLUSTER} == "eks" ]]; then
   git apply tetrateci/patches/eks/eks-ingress.1.7.patch
 fi
 
-PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster)
+PACKAGES=$(go list ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster)
 
 echo "Starting Testing"
+
+export GODEBUG=x509ignoreCN=0
 
 for package in $PACKAGES; do
   n=0
   until [ "$n" -ge 3 ]
   do
     echo "========================================================TESTING $package | TRY $n========================================================"
-    go test -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
+    go test -test.v $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
     for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
     n=$((n+1))
   done
