469bbcc713d3236bbc13171a985b419d8d1ba547
Devarajan Ramaswamy <deva.ramaswamy@tetrate.io>
2021-03-11 22:28:20 +0530

    merge with tetrate-workflow

--- tetrateci/cleanup_eks_cluster.sh
+++ tetrateci/cleanup_eks_cluster.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+./tetrateci/version_check.py && exit
+
 set -o errexit
 set -o pipefail
+
 SHA8=$(git rev-parse --short $GITHUB_SHA)
-SUFFIX=$(sed 's/\.//g' <<< $VER)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
 echo "Deleting eks cluster $CLUSTER_NAME"
 eksctl delete cluster --name $CLUSTER_NAME
---
+++ tetrateci/create_eksd_cluster.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+if [[ ! -f ~/.aws/config && ! -f ~/.aws/credentials ]]
+then
+    echo "warn: didn't find config and credentials in ~/.aws."
+    echo "checking for environment varibles...."
+    if [[ ! -v AWS_ACCESS_KEY_ID && ! -v AWS_SECRET_ACCESS_KEY ]]
+    then
+        echo "error: neither is aws_access_key_id and aws_secret_access_key is set."
+        exit 2
+    fi
+fi
+
+SHA8=$(git rev-parse --short $GITHUB_SHA)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
+
+## Cluster name has to end with k8s.local
+CLUSTER_NAME="test-istio-$SHA8-$SUFFIX.k8s.local"
+
+cd $BASEDIR/..
+git clone https://github.com/aws/eks-distro.git
+cd eks-distro/development/kops
+
+export KOPS_STATE_STORE=s3://${S3_BUCKET}
+export KOPS_CLUSTER_NAME=${CLUSTER_NAME}
+
+cp $BASEDIR/tetrateci/eks-d.tpl .
+
+# possible versions: 1-18, 1-19
+export RELEASE_BRANCH=$(sed 's/\./-/g' <<< $K8S_VERSION)
+
+echo "creating a eksd cluster with \"$CLUSTER_NAME\" name..."
+./run_cluster.sh
+
+#Wait for the cluster to be created
+./cluster_wait.sh
+
+cd $BASEDIR
