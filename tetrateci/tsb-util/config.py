dfc62c8011fed0cd9b60486e1f9742524098a608
Kuntal Majumder <kuntal@infracloud.io>
2021-04-12 13:11:51 +0530

    add more customizibality to the configuration

--- tetrateci/tsb-util/config.py
+++ tetrateci/tsb-util/config.py
@@ -1,38 +1,59 @@
 import yaml
 from dataclasses import dataclass
 from typing import List
+@dataclass
+class productpage:
+    context: str
+    gateway_yaml: str
 
 @dataclass
-class cluster_config:
-    instances: int
+class reviewspage:
     context: str
+    virtualservice_yaml: str
+    destinationrules_yaml: str
+
+@dataclass
+class detailspage:
+    context: str
+
+@dataclass
+class bookinfo:
+    replicas: int
+    context: str
+    product: productpage
+    reviews: reviewspage
+    details: detailspage
 
 def parse_config(yaml_dict):
     parsed_conf = []
     for config in yaml_dict["config"]:
         # context is not necessary, we can always fallback to current context
-        conf = cluster_config(config["instances"], config.get("context"))
+        product = productpage(
+            config["product"].get("context"), config["product"]["gatewayYaml"]
+        )
+        reviews = reviewspage(
+            config["reviews"].get("context"),
+            config["reviews"]["virtualServiceYaml"],
+            config["reviews"]["destinationRulesYaml"],
+        )
+        details = config.get("details")
+        if details is not None:
+            details = detailspage(config["details"].get("context"))
+        conf = bookinfo(
+            config["replicas"], config.get("context"), product, reviews, details
+        )
         parsed_conf.append(conf)
     return parsed_conf
 
 def read_config_yaml(filename):
-    with open(filename) as file :
+    with open(filename) as file:
         iop_config = yaml.load(file, Loader=yaml.FullLoader)
         return parse_config(iop_config)
 
 def modify_gateway(filename, hostname):
-    with open(filename) as file :
+    with open(filename) as file:
         config = list(yaml.load_all(file, Loader=yaml.FullLoader))
         networking_config = config[0]
         networking_config["spec"]["servers"][0]["hosts"][0] = hostname
         f = open(filename, "w")
         yaml.dump_all(config, f)
-
-def modify_virtual_service(filename):
-    with open(filename) as file :
-        config = yaml.load(file, Loader=yaml.FullLoader)
-        config["spec"]["http"][0]['route'][0]["weight"] = 20
-        route = {'destination': {'host': 'reviews', 'subset': 'v2'}, 'weight': 30}
-        config["spec"]["http"][0]['route'].append(route)
-        f = open(filename, "w")
-        yaml.dump(config, f)
--- tetrateci/tsb-util/tsb_util.py
+++ tetrateci/tsb-util/tsb_util.py
@@ -6,11 +6,7 @@ from subprocess import PIPE, Popen
 
 # https://stackoverflow.com/a/23796709
 def cmdline(command):
-    process = Popen(
-        args=command,
-        stdout=PIPE,
-        shell=True
-    )
+    process = Popen(args=command, stdout=PIPE, shell=True)
     return process.communicate()[0]
 
 def print_cmdline(command):
@@ -18,20 +14,19 @@ def print_cmdline(command):
 
 cleanup_script = ""
 
-def install_bookinfo(bookinfo_instances):
+def install_bookinfo(conf):
     global cleanup_script
-    bookinfo_instances *= 4
+
     download_url = "https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml"
     urllib.request.urlretrieve(download_url, "/tmp/bookinfo.yaml")
-    services = ["ratings", "details", "reviews", "productpage"]
     base_cmd = "kubectl apply -f /tmp/bookinfo.yaml -n "
 
-    #virtual_service = "/tmp/" + folder_name + "/samples/bookinfo/networking/virtual-service-reviews-50-v3.yaml"
-    #config.modify_virtual_service(virtual_service)
+    # virtual_service = "/tmp/" + folder_name + "/samples/bookinfo/networking/virtual-service-reviews-50-v3.yaml"
+    # config.modify_virtual_service(virtual_service)
 
     i = 0
 
-    while i < bookinfo_instances:
+    while i < conf.replicas * 3:
         print("Installing Bookinfo")
 
         ns = "bookinfo" + str(i)
@@ -40,29 +35,62 @@ def install_bookinfo(bookinfo_instances):
         print_cmdline("kubectl label namespace " + ns + " istio-injection=enabled")
         cleanup_script += "kubectl delete ns " + ns + "\n"
 
-        print("Installing " + services[i%4])
-        print_cmdline(base_cmd + ns + " -l account=" + services[i%4])
-        print_cmdline(base_cmd + ns + " -l app=" + services[i%4])
+        print("Installing details")
+        print_cmdline(base_cmd + ns + " -l account=details")
+        print_cmdline(base_cmd + ns + " -l app=details")
 
-        if services[i%4] == "productpage":
-            svc_domain = ".svc.cluster.local"
-            ratings_env = "RATINGS_HOSTNAME=ratings.bookinfo" + str(i-3) + svc_domain
-            details_env = "DETAILS_HOSTNAME=details.bookinfo"+ str(i-2) + svc_domain
-            reviews_env = "REVIEWS_HOSTNAME=reviews.bookinfo"+ str(i-1) + svc_domain
-            cmd = "kubectl set env deployments productpage-v1 -n " + ns + " " + ratings_env + " " + details_env + " " + reviews_env
-            print_cmdline(cmd)
-            #gateway_file = "/tmp/"+ folder_name +"/samples/bookinfo/networking/bookinfo-gateway.yaml"
-            #hostname = ns + ".k8s.local"
-            #config.modify_gateway(gateway_file, hostname)
-            #cmd = "kubectl apply -f " + gateway_file + " -n " + ns
-            #os.system(cmd)
+        i += 1
 
-        if services[i%4] == "reviews":
-            #cmd = "kubectl apply -f " + virtual_service + " -n " + ns
-            #os.system(cmd)
-            #cmd = "kubectl apply -f /tmp/" + folder_name + "/samples/bookinfo/networking/destination-rule-reviews.yaml -n " + ns
-            #os.system(cmd)
-            pass
+        ns = "bookinfo" + str(i)
+        print("Create Namespace : " + ns)
+        print_cmdline("kubectl create ns " + ns)
+        print_cmdline("kubectl label namespace " + ns + " istio-injection=enabled")
+        cleanup_script += "kubectl delete ns " + ns + "\n"
+
+        print("Installing reviews and ratings")
+        print_cmdline(base_cmd + ns + " -l account=reviews")
+        print_cmdline(base_cmd + ns + " -l app=reviews")
+        print_cmdline(base_cmd + ns + " -l account=ratings")
+        print_cmdline(base_cmd + ns + " -l app=ratings")
+        print_cmdline(
+            "kubectl apply -f " + conf.reviews.virtualservice_yaml + " -n " + ns
+        )
+        print_cmdline(
+            "kubectl apply -f " + conf.reviews.destinationrules_yaml + " -n " + ns
+        )
+
+        i += 1
+
+        ns = "bookinfo" + str(i)
+        print("Create Namespace : " + ns)
+        print_cmdline("kubectl create ns " + ns)
+        print_cmdline("kubectl label namespace " + ns + " istio-injection=enabled")
+        cleanup_script += "kubectl delete ns " + ns + "\n"
+        print("Installing productpage")
+        print_cmdline(base_cmd + ns + " -l account=productpage")
+        print_cmdline(base_cmd + ns + " -l app=productpage")
+
+        svc_domain = ".svc.cluster.local"
+        details_env = "DETAILS_HOSTNAME=details.bookinfo" + str(i - 2) + svc_domain
+        reviews_env = "REVIEWS_HOSTNAME=reviews.bookinfo" + str(i - 1) + svc_domain
+        ratings_env = "RATINGS_HOSTNAME=ratings.bookinfo" + str(i - 1) + svc_domain
+        cmd = (
+            "kubectl set env deployments productpage-v1 -n "
+            + ns
+            + " "
+            + ratings_env
+            + " "
+            + details_env
+            + " "
+            + reviews_env
+        )
+        print_cmdline(cmd)
+
+        gateway_file = conf.product.gateway_yaml
+        hostname = ns + ".k8s.local"
+        config.modify_gateway(gateway_file, hostname)
+        cmd = "kubectl apply -f " + gateway_file + " -n " + ns
+        print_cmdline(cmd)
 
         i += 1
 
@@ -89,7 +117,7 @@ def main():
             print_cmdline(cmd)
             cleanup_script += cmd + "\n"
 
-        install_bookinfo(conf.instances)
+        install_bookinfo(conf)
 
     f = open("./cleanup.sh", "w")
     f.write(cleanup_script)
