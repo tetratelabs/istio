1f7022a3a65f69ae928ce9a46b5792b2b45320fe
Kuntal Majumder <kuntal@infracloud.io>
2021-04-28 10:27:59 +0530

    remove unused function

--- tetrateci/tsb-util/config.py
+++ tetrateci/tsb-util/config.py
@@ -32,13 +32,3 @@ def read_config_yaml(filename):
     with open(filename) as file:
         iop_config = yaml.load(file, Loader=yaml.FullLoader)
         return parse_config(iop_config)
-
-def modify_gateway(filename, key):
-    with open(filename) as file:
-        template = Template(file.read())
-        complete_yaml = template.render(
-            gatewayName=key + "-gateway",
-            hostname=key + ".k8s.local",
-            secretName=key + "-credential",
-        )
-        return complete_yaml
