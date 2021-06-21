ba6fe5024243c7dcc26907b48219838f7f71ce8e
Kuntal Majumder <kuntal@infracloud.io>
2021-06-21 13:35:01 +0530

    move all cert related stuff to certs.py

--- tetrateci/tsb-util/bookinfo-single-gw.py
+++ tetrateci/tsb-util/bookinfo-single-gw.py
@@ -2,10 +2,10 @@ import os
 from jinja2 import Template
 import yaml
 import base64
-import copy
 import argparse
 from dataclasses import dataclass
 from marshmallow_dataclass import class_schema
+import certs
 
 @dataclass
 class config:
@@ -26,77 +26,6 @@ def save_file(fname, content):
     f = open(fname, "w")
     f.write(content)
     f.close()
-
-def create_cert():
-    os.makedirs("cert/", exist_ok=True)
-    if not os.path.exists("cert/tetrate.test.com.crt") or not os.path.exists(
-        "cert/tetrate.test.com.key"
-    ):
-        os.system(
-            "openssl req -x509 -sha256 -nodes -days 365 \
-        -newkey rsa:4096 -subj '/C=US/ST=CA/O=Tetrateio/CN=tetrate.test.com' \
-        -keyout cert/tetrate.test.com.key -out cert/tetrate.test.com.crt"
-        )
-
-    if not os.path.exists("cert/wildcard.tetrate.test.com.csr") or not os.path.exists(
-        "cert/wildcard.tetrate.test.com.key"
-    ):
-        os.system(
-            "openssl req -out cert/wildcard.tetrate.test.com.csr -newkey rsa:2048 \
-        -nodes -keyout cert/wildcard.tetrate.test.com.key \
-        -subj '/CN=*.tetrate.test.com/O=bookinfo organization'"
-        )
-
-    if not os.path.exists("cert/wildcard.tetrate.test.com.crt"):
-        os.system(
-            "openssl x509 -req -sha256 -days 365 -CA cert/tetrate.test.com.crt \
-        -CAkey cert/tetrate.test.com.key \
-        -set_serial 0 -in cert/wildcard.tetrate.test.com.csr \
-        -out cert/wildcard.tetrate.test.com.crt"
-        )
-
-def create_secret(ns, fname):
-    secret_name = "wilcard-credential"
-    hostname = "wildcard.tetrate.test.com"
-    keyfile = open("cert/" + hostname + ".key")
-    certfile = open("cert/" + hostname + ".crt")
-
-    t = open(script_path + "/templates/k8s-objects/secret.yaml")
-    template = Template(t.read())
-    r = template.render(
-        name=secret_name,
-        ns=ns,
-        crtData=base64.b64encode(certfile.read().encode("utf-8")).decode("utf-8"),
-        keyData=base64.b64encode(keyfile.read().encode("utf-8")).decode("utf-8"),
-    )
-    t.close()
-
-    f = open(fname, "w")
-    f.write(r)
-    f.close()
-
-    keyfile.close()
-    certfile.close()
-
-def create_trafficgen_secret(ns, fname):
-    secret_name = ns + "-ca-cert"
-    certfile = open("cert/tetrate.test.com.crt")
-
-    t = open(script_path + "/templates/k8s-objects/trafficgen-secret.yaml")
-    template = Template(t.read())
-    r = template.render(
-        name=secret_name,
-        ns=ns,
-        data=base64.b64encode(certfile.read().encode("utf-8")).decode("utf-8"),
-    )
-    t.close()
-
-    f = open(fname, "w")
-    f.write(r)
-    f.close()
-
-    certfile.close()
-    return secret_name
 
 def install_bookinfo(ns, key):
     svc_domain = ".svc.cluster.local"
@@ -220,9 +149,11 @@ def main():
     t.close()
     save_file("generated/k8s-objects/ingress.yaml", r)
 
-    create_cert()
-    create_secret(namespace, "generated/k8s-objects/secret.yaml")
-    create_trafficgen_secret(namespace, "generated/k8s-objects/trafficgen-secret.yaml")
+    certs.generate_wildcard_cert()
+    certs.create_wildcard_secret(namespace, "generated/k8s-objects/secret.yaml")
+    certs.create_trafficgen_wildcard_secret(
+        namespace, "generated/k8s-objects/trafficgen-secret.yaml"
+    )
 
     http_routes = []
     curl_calls = []
