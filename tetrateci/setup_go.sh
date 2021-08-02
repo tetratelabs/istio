397f13cc6ef86eefd6e3d5959604704894483420
mathetake <github-actions@github.com>
2021-08-02 05:19:10 +0000

    Merging tetrate-workflow with tetrate-release-1.9

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -13,10 +13,9 @@ if $(grep -q "1.10" <<< $TAG); then
     export GOLANG_VERSION=1.16.4
 fi
 
-echo "Fetching Go $GOLANG_VERSION"
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
 
-wget -O go.tgz "$url"
+wget -q -O go.tgz "$url"
 
 sudo tar -C /usr/local -xzf go.tgz
 rm go.tgz
