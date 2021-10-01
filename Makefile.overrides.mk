1504bda4843ef5e39e0c10313824f9cbc6d45e45
Takaya Saeki <takaya@tetrate.io>
2021-10-01 20:08:25 +0900

    Add comment which explains why overrides are necessary

--- Makefile.overrides.mk
+++ Makefile.overrides.mk
@@ -24,6 +24,7 @@ CONTAINER_OPTIONS = --mount type=bind,source=/tmp,destination=/tmp --net=host
 ifeq ($(PROXY_REPO_SHA),)
   export PROXY_REPO_SHA:=$(shell grep PROXY_REPO_SHA istio.deps  -A 4 | grep lastStableSHA | cut -f 4 -d '"')
 endif
+# We override these variables to build envoy with our modsecurity filter, and push it to our private storage.
 export ISTIO_ENVOY_BASE_URL ?= gs://tetrate-istio-build/proxy
 export ISTIO_ENVOY_RELEASE_URL ?= $(ISTIO_ENVOY_BASE_URL)/envoy-alpha-modsecurity-$(PROXY_REPO_SHA).tar.gz
 
