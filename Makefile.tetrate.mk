d8a03f1dc7addeb7494f6a85252c5bfc1566c71e
Takaya Saeki <takaya@tetrate.io>
2021-10-11 15:58:33 +0900

    Update the GCS URL with the new location

--- Makefile.tetrate.mk
+++ Makefile.tetrate.mk
@@ -6,6 +6,6 @@ ifeq ($(PROXY_REPO_SHA),)
 endif
 
 # We override these variables to build envoy with our modsecurity filter, and push it to our private storage.
-export ISTIO_ENVOY_BASE_URL ?= gs://tetrate-istio-build/proxy
+export ISTIO_ENVOY_BASE_URL ?= gs://tetrate-internal-istio-build/proxy
 export ISTIO_ENVOY_RELEASE_URL ?= $(ISTIO_ENVOY_BASE_URL)/envoy-alpha-modsecurity-$(PROXY_REPO_SHA).tar.gz
 export TETRATE_MODSECURITYDEPS_RELEASE_URL ?= $(ISTIO_ENVOY_BASE_URL)/modsecurity-plugin-deps-$(PROXY_REPO_SHA).tar.gz
