a015e830f3244e2fbcbc412250a30cbad2cb2bcb
danehans <daneyonhansen@gmail.com>
2022-01-05 15:28:52 -0800

    Updates go setup scripts for Istio 1.12

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -14,7 +14,7 @@ if $(grep -q "1.10" <<< $TAG || grep -q "1.11" <<< $TAG); then
     export GOLANG_VERSION=1.16.9
 fi
 
-if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.12" ]]; then
+if $(grep -q "1.12" <<< $TAG); then
     export GOLANG_VERSION=1.17.3
 fi
 
