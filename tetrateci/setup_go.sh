db4b1afd7e16cbfe3726a0afaed1c00b8e352796
Yaroslav Skopets <yaroslav@tetrate.io>
2022-01-06 12:12:49 +0100

    Merging tetrate-workflow with tetrate-release-1.12

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -u
 
 if $(grep -q "1.7" <<< $TAG); then
     export GOLANG_VERSION=1.14.12
@@ -11,6 +12,10 @@ fi
 
 if $(grep -q "1.10" <<< $TAG || grep -q "1.11" <<< $TAG); then
     export GOLANG_VERSION=1.16.9
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.12" ]]; then
+    export GOLANG_VERSION=1.17.3
 fi
 
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
