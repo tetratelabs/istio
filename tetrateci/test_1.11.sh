b24bba351a4f54aa74e7efc8e37bbc445901e40f
Yaroslav Skopets <yaroslav@tetrate.io>
2022-02-09 18:27:18 +0100

    test security
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- tetrateci/test_1.11.sh
+++ tetrateci/test_1.11.sh
@@ -33,7 +33,7 @@ if [[ "${CLUSTER}" == "eks" ]]; then
   git apply "${SCRIPTDIR}/patches/eks/eks-ingress.1.11.patch"
 fi
 
-PACKAGES=$(go list -tags=integ "${ROOTDIR}/tests/integration/..." | grep -e pilot/endpointslice)
+PACKAGES=$(go list -tags=integ "${ROOTDIR}/tests/integration/..." | grep -e security)
 
 echo "Starting Testing"
 
