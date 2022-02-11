932144c40efbeb657c2b413f24f8aa1c889d21fa
Yaroslav Skopets <yaroslav@tetrate.io>
2022-02-11 11:52:26 +0100

    try changing Go version back to 1.16
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -15,7 +15,7 @@ if $(grep -q "1.10" <<< $TAG); then
 fi
 
 if $(grep -q "1.11" <<< $TAG); then
-    export GOLANG_VERSION=1.17.6
+    export GOLANG_VERSION=1.16.14
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.12" ]]; then
