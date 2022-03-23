c277c466cf483f0f246ccfabea6581262285cf21
psbrar99 <86856448+psbrar99@users.noreply.github.com>
2022-03-23 09:55:45 -0600

    1.13 fix (#667)
    * added bom utility as build is failing when creating sbom
    
    * using specific version of bom
    
    * checking

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -19,7 +19,9 @@ fi
 
 # BOM is needed for generating bill of materials, required by Istio since 1.13, https://github.com/istio/release-builder/pull/893
 go install sigs.k8s.io/bom/cmd/bom@v0.2.2
-cp go/bin/bom /usr/local/bin/
+pwd
+go env
+cp /home/runner/go/bin/bom /usr/local/bin/
 
 sudo gem install fpm
 sudo apt-get install go-bindata -y
