b6e43a1a10e66cecd9eeb4492be23bdb4153d320
psbrar99 <github-actions@github.com>
2022-03-23 15:56:38 +0000

    Merging tetrate-workflow with tetrate-release-1.7

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -16,6 +16,13 @@ else
 fi
 
 ## Set up release-builder
+
+# BOM is needed for generating bill of materials, required by Istio since 1.13, https://github.com/istio/release-builder/pull/893
+go install sigs.k8s.io/bom/cmd/bom@v0.2.2
+pwd
+go env
+cp /home/runner/go/bin/bom /usr/local/bin/
+
 sudo gem install fpm
 sudo apt-get install go-bindata -y
 export BRANCH=release-${REL_BRANCH_VER}
