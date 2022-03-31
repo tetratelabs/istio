ad18f539242f0e11142ea6fe5758874331605541
psbrar99 <brargg1989@gmail.com>
2022-03-31 11:39:07 -0600

    removing mkdir step

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -106,7 +106,6 @@ sudo rm -rf /tmp/istio-release/sources/
 # If RELEASE, Build Archives
 if [[ -z ${TEST:-} ]]; then
     echo "Building archives..."
-    mkdir /tmp/istio-release
 	# if FIPS, need to use native go as boringgo as of now can't build archives for different platforms
     if [[ ${TAG} =~ "fips" ]]; then
         sudo rm -rf /usr/local/go
