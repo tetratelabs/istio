7320d05fb28c11945c440a8e2ae9577fc53ab6e3
root <root@instance-1.c.gke-istio-test-psb.internal>
2022-04-05 17:40:33 +0000

    ingress_patch changes

--- tetrateci/test_1.13.sh
+++ tetrateci/test_1.13.sh
@@ -37,7 +37,7 @@ fi
 
 if [[ "${CLUSTER}" == "eks" ]]; then
   echo "Applying Ingress patch for EKS...."
-  git apply "${SCRIPTDIR}/patches/eks/eks-ingress.1.11.patch"
+  git apply "${SCRIPTDIR}/patches/eks/eks-ingress.1.13.patch"
 fi
 
 PACKAGES=$(go list -tags=integ "${ROOTDIR}/tests/integration/...")
