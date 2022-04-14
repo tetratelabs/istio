9e140ecef67437d5c762e62a1d526d6e1b269617
psbrar99 <brargg1989@gmail.com>
2022-04-14 14:18:07 -0600

    bumping go version to 1.17.8 for 1.12.6 release

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -14,12 +14,12 @@ if $(grep -q "1.10" <<< $TAG); then
   export GOLANG_VERSION=1.16.9b7
 fi
 
-if $(grep -q "1.11" <<< $TAG || grep -q "1.12" <<< $TAG); then
-  export GOLANG_VERSION=1.17.6b7
+if $(grep -q "1.11" <<< $TAG); then
+    export GOLANG_VERSION=1.17.6b7
 fi
 
-if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
-  export GOLANG_VERSION=1.17.8b7
+if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
+    export GOLANG_VERSION=1.17.8b7
 fi
 
 url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
