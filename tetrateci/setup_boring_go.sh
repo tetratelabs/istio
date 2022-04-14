2c4b9cbf7aa7f905f19084021e2cda4ce8d565fb
psbrar99 <brargg1989@gmail.com>
2022-04-14 14:21:17 -0600

    Remove extra spaces

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -15,11 +15,11 @@ if $(grep -q "1.10" <<< $TAG); then
 fi
 
 if $(grep -q "1.11" <<< $TAG); then
-    export GOLANG_VERSION=1.17.6b7
+   export GOLANG_VERSION=1.17.6b7
 fi
 
 if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
-    export GOLANG_VERSION=1.17.8b7
+   export GOLANG_VERSION=1.17.8b7
 fi
 
 url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
