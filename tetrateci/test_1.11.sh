53015b00d651325821916ee05d5f613745811ee3
Lizan Zhou <lizan@tetrate.io>
2022-04-18 13:39:27 +0900

    1.13 patch
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- tetrateci/test_1.11.sh
+++ tetrateci/test_1.11.sh
@@ -1,46 +1,95 @@
 #!/usr/bin/env bash
-./tetrateci/version_check.py && exit
-set -e
+#
+# Copyright (c) Tetrate, Inc 2022 All Rights Reserved.
 
-source ./tetrateci/setup_go.sh
+set -e
+set -u
+set -x
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOTDIR=$( cd "${SCRIPTDIR}/.." && pwd )
+
+"${SCRIPTDIR}/version_check.py" && exit
+
+# shellcheck disable=SC1091
+source "${SCRIPTDIR}/setup_go.sh"
+
+COMMON_TEST_FLAGS=()
 
 echo "Applying patches...."
 
-git apply tetrateci/patches/common/increase-dashboard-timeout.1.11.patch
+# Apply the same patches that were applies when building test images
+"${SCRIPTDIR}/apply_e2e_build_patches.sh"
 
-if [[ ${CLUSTER} == "gke" ]]; then
+git apply "${SCRIPTDIR}/patches/common/increase-dashboard-timeout.1.11.patch"
+
+if [[ "${CLUSTER}" == "gke" ]]; then
   echo "Generating operator config for GKE"
+
   # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
-  python3 -m pip install pyyaml --user && ./tetrateci/gen_iop.py
-  CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
+  python3 -m pip install pyyaml --user && python3 "${SCRIPTDIR}/gen_iop.py"
+
+  COMMON_TEST_FLAGS+=( "-istio.test.kube.helm.iopFile=${SCRIPTDIR}/iop-gke-integration.yml" )
 
   echo "Applying GKE specific patches...."
-  git apply tetrateci/patches/gke/chiron-gke.patch
+  git apply "${SCRIPTDIR}/patches/gke/chiron-gke.patch"
 fi
 
-if [[ ${CLUSTER} == "eks" ]]; then
+if [[ "${CLUSTER}" == "eks" ]]; then
   echo "Applying Ingress patch for EKS...."
-  git apply tetrateci/patches/eks/eks-ingress.1.11.patch
+  git apply "${SCRIPTDIR}/patches/eks/eks-ingress.1.11.patch"
 fi
 
-if $(grep -q "1.17" <<< ${K8S_VERSION}); then
-  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /endpointslice | grep -v /stackdriver)
-else
-  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /stackdriver)
-fi
+PACKAGES=$(go list -tags=integ "${ROOTDIR}/tests/integration/...")
 
 echo "Starting Testing"
 
-for package in $PACKAGES; do
-  n=0
-  until [ "$n" -ge 3 ]
-  do
-    echo "========================================================TESTING $package | TRY $n========================================================"
-    go test -test.v -tags=integ $package -timeout 2h --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
-    for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
-    n=$((n+1))
+FAILED_PACKAGES=()
+
+for pkg in $PACKAGES; do
+  echo "========================================================TESTING ${pkg} ========================================================"
+
+  SKIP_RULE=$( grep -F "${pkg}=" "${SCRIPTDIR}/${ISTIO_MINOR_VER}/test/skip.d/${CLUSTER}" 2>/dev/null || echo "" )
+  SKIP_TESTS=$( echo -n "${SKIP_RULE#${pkg}=}" )
+
+  if [[ "${SKIP_TESTS}" == "*" ]]; then
+    echo "Skipping according to the rule: ${SKIP_RULE}"
+    continue
+  fi
+
+  read -ra SKIP_TESTS_ARRAY <<< "${SKIP_TESTS}"
+
+  SKIP_TEST_FLAGS=()
+  for test in ${SKIP_TESTS_ARRAY[@]+"${SKIP_TESTS_ARRAY[@]}"} ; do
+    SKIP_TEST_FLAGS+=( "--istio.test.skip=${test}" )
   done
-  [ "$n" -ge 3 ] && exit 1
+
+  go test \
+    -test.v \
+    -timeout 2h \
+    -tags=integ \
+    "${pkg}" \
+    --istio.test.select=-postsubmit,-flaky \
+    ${SKIP_TEST_FLAGS[@]+"${SKIP_TEST_FLAGS[@]}"} \
+    --istio.test.ci \
+    --istio.test.pullpolicy=IfNotPresent \
+    --istio.test.retries=1 \
+    ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
+    || \
+    { FAILED_PACKAGES+=( "${pkg}" ) && echo "Test Failed: ${pkg}" ; }
+
+  find /tmp -mindepth 1 -maxdepth 1 -type d -name '*istio*' -exec sudo rm -f -- {} \;
 done
 
 echo "Testing Done"
+
+if [[ ${#FAILED_PACKAGES[@]} -gt 0 ]]; then
+  echo ""
+  echo "Some of the tests have failed :("
+  echo ""
+  echo "Packages with failed tests:"
+  for pkg in "${FAILED_PACKAGES[@]}"; do
+    echo "- ${pkg}"
+  done
+  exit 1
+fi
