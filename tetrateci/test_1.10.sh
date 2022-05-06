b155dc1eeb6b575c3498c25ef071d2188d002e57
Nishant Hiremath <nishant.hiremath@infracloud.io>
2022-05-06 18:57:55 +0530

    minor update in test_1.10.sh

--- tetrateci/test_1.10.sh
+++ tetrateci/test_1.10.sh
@@ -1,95 +1,55 @@
 #!/usr/bin/env bash
-#
-# Copyright (c) Tetrate, Inc 2022 All Rights Reserved.
-
+./tetrateci/version_check.py && exit
 set -e
-set -u
-set -x
 
-SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ROOTDIR=$( cd "${SCRIPTDIR}/.." && pwd )
-
-"${SCRIPTDIR}/version_check.py" && exit
-
-# shellcheck disable=SC1091
-source "${SCRIPTDIR}/setup_go.sh"
-
-COMMON_TEST_FLAGS=()
+source ./tetrateci/setup_go.sh
 
 echo "Applying patches...."
 
-# Apply the same patches that were applies when building test images
-"${SCRIPTDIR}/apply_e2e_build_patches.sh"
+# git apply tetrateci/patches/common/increase-vm-timeout.1.9.patch
+# git apply tetrateci/patches/common/increase-sniffing-timeout.1.9.patch
+git apply tetrateci/patches/common/increase-dashboard-timeout.1.10.patch
+git apply tetrateci/patches/common/disable-vmregistration.1.10.patch # https://github.com/istio/istio/issues/29100
+git apply tetrateci/patches/common/disable-passthroughfilterchain.1.10.patch # https://github.com/istio/istio/issues/32623
 
-git apply "${SCRIPTDIR}/patches/common/increase-dashboard-timeout.1.10.patch"
+# the code fails whenever there is something other than digits in the k8s minor version
+# in our case which is a "+" symbol due to extra patching by corresponding vendor
+# so we get 1.17+ instead of 1.17
+# git apply tetrateci/patches/common/fix-version-check.1.9.patch
 
-if [[ "${CLUSTER}" == "gke" ]]; then
+if [[ ${CLUSTER} == "gke" ]]; then
   echo "Generating operator config for GKE"
-
   # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
-  python3 -m pip install pyyaml --user && python3 "${SCRIPTDIR}/gen_iop.py"
-
-  COMMON_TEST_FLAGS+=( "-istio.test.kube.helm.iopFile=${SCRIPTDIR}/iop-gke-integration.yml" )
+  python3 -m pip install pyyaml --user && ./tetrateci/gen_iop.py
+  CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
 
   echo "Applying GKE specific patches...."
-  git apply "${SCRIPTDIR}/patches/gke/chiron-gke.patch"
+  git apply tetrateci/patches/gke/chiron-gke.patch
 fi
 
-if [[ "${CLUSTER}" == "eks" ]]; then
+if [[ ${CLUSTER} == "eks" ]]; then
   echo "Applying Ingress patch for EKS...."
-  git apply "${SCRIPTDIR}/patches/eks/eks-ingress.1.10.patch"
+  git apply tetrateci/patches/eks/eks-ingress.1.10.patch
 fi
 
-PACKAGES=$(go list -tags=integ "${ROOTDIR}/tests/integration/...")
+if $(grep -q "1.17" <<< ${K8S_VERSION}); then
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /endpointslice | grep -v /stackdriver)
+else
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /stackdriver)
+fi
 
 echo "Starting Testing"
 
-FAILED_PACKAGES=()
-
-for pkg in $PACKAGES; do
-  echo "========================================================TESTING ${pkg} ========================================================"
-
-  SKIP_RULE=$( grep -F "${pkg}=" "${SCRIPTDIR}/${ISTIO_MINOR_VER}/test/skip.d/${CLUSTER}" 2>/dev/null || echo "" )
-  SKIP_TESTS=$( echo -n "${SKIP_RULE#${pkg}=}" )
-
-  if [[ "${SKIP_TESTS}" == "*" ]]; then
-    echo "Skipping according to the rule: ${SKIP_RULE}"
-    continue
-  fi
-
-  read -ra SKIP_TESTS_ARRAY <<< "${SKIP_TESTS}"
-
-  SKIP_TEST_FLAGS=()
-  for test in ${SKIP_TESTS_ARRAY[@]+"${SKIP_TESTS_ARRAY[@]}"} ; do
-    SKIP_TEST_FLAGS+=( "--istio.test.skip=${test}" )
+for package in $PACKAGES; do
+  n=0
+  until [ "$n" -ge 3 ]
+  do
+    echo "========================================================TESTING $package | TRY $n========================================================"
+    go test -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent && break || echo "Test Failed: $package"
+    for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
+    n=$((n+1))
   done
-
-  go test \
-    -test.v \
-    -timeout 2h \
-    -tags=integ \
-    "${pkg}" \
-    --istio.test.select=-postsubmit,-flaky \
-    ${SKIP_TEST_FLAGS[@]+"${SKIP_TEST_FLAGS[@]}"} \
-    --istio.test.ci \
-    --istio.test.pullpolicy=IfNotPresent \
-    --istio.test.retries=1 \
-    ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
-    || \
-    { FAILED_PACKAGES+=( "${pkg}" ) && echo "Test Failed: ${pkg}" ; }
-
-  find /tmp -mindepth 1 -maxdepth 1 -type d -name '*istio*' -exec sudo rm -f -- {} \;
+  [ "$n" -ge 3 ] && exit 1
 done
 
 echo "Testing Done"
-
-if [[ ${#FAILED_PACKAGES[@]} -gt 0 ]]; then
-  echo ""
-  echo "Some of the tests have failed :("
-  echo ""
-  echo "Packages with failed tests:"
-  for pkg in "${FAILED_PACKAGES[@]}"; do
-    echo "- ${pkg}"
-  done
-  exit 1
-fi
