6cf4986e09e531e78ac5307aadcd1aa9176f6380
psbrar99 <brargg1989@gmail.com>
2022-05-09 18:33:43 -0600

    adding missing scripts

---
+++ tetrateci/create_aks_cluster.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+./tetrateci/version_check.py && exit
+
+set -o errexit
+set -o pipefail
+
+SHA8=$(git rev-parse --short $GITHUB_SHA)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
+CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
+
+echo "Fetching location of the resource"
+location=$(az group show -g $RESOURCE | jq '.location')
+
+echo "Fetching available kubernetes patch version for $K8S_VERSION"
+version=$(az aks get-versions -l $location | jq '.orchestrators[] | .orchestratorVersion' | grep $K8S_VERSION | tail -n 1 | tr -d '"')
+
+echo "Kubernetes version selected: $version"
+
+az aks create --resource-group $RESOURCE --name $CLUSTER_NAME --node-count 3 --generate-ssh-keys --kubernetes-version $version -s standard_ds3_v2 --network-plugin azure
+az aks get-credentials --resource-group $RESOURCE --name $CLUSTER_NAME
---
+++ tetrateci/gen_iop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import yaml
+
+extra = {'cni' : {'cniBinDir' : '/home/kubernetes/bin', 'excludeNamespaces' : ['istio-system', 'kube-system']}}
+
+with open(r'./tests/integration/iop-integration-test-defaults.yaml') as file :
+    iop_config = yaml.load(file, Loader=yaml.FullLoader)
+    iop_config['spec']['values'].update(extra)
+    f = open(r'./tetrateci/iop-gke-integration.yml', 'w')
+    yaml.dump(iop_config, f)
