629e9122c1e307c92df0b1bc774ddfc822c2ebd1
psbrar99 <brargg1989@gmail.com>
2022-05-10 18:55:31 -0600

    fips testcases

--- tetrateci/test_1.13.sh
+++ tetrateci/test_1.13.sh
@@ -71,7 +71,7 @@ for pkg in $PACKAGES; do
     ${SKIP_TEST_FLAGS[@]+"${SKIP_TEST_FLAGS[@]}"} \
     --istio.test.ci \
     --istio.test.hub=${HUB} \
-    --istio.test.tag=${TAG}-distroless
+    --istio.test.tag=${TAG}-distroless \
     --istio.test.pullpolicy=IfNotPresent \
     --istio.test.retries=1 \
     ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
