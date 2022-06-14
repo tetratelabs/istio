84ea1f7f9aaa8926b9a58a8d9fe60c5ac73f490e
Tejas Shekokar <shekokar.tejas@gmail.com>
2022-06-14 20:46:45 +0530

    bump up the go version

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -18,8 +18,12 @@ if $(grep -q "1.11" <<< $TAG); then
     export GOLANG_VERSION=1.17.6
 fi
 
-if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]] || $(grep -q "1.14" <<< $TAG); then
+if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
     export GOLANG_VERSION=1.17.8
+fi
+
+if $(grep -q "1.14" <<< $TAG); then
+    export GOLANG_VERSION=1.18.2
 fi
 
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
