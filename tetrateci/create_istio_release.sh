2d98e4fb3688c234f17a8b9b1ae8e208ddf9828f
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-07-07 22:43:28 -0600

    Update create_istio_release.sh

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -18,7 +18,7 @@ fi
 ## Set up release-builder
 
 # BOM is needed for generating bill of materials, required by Istio since 1.13, https://github.com/istio/release-builder/pull/893
-go install sigs.k8s.io/bom/cmd/bom@v0.2.2
+go install sigs.k8s.io/bom/cmd/bom@v0.3.0
 sudo cp /home/runner/go/bin/bom /usr/local/bin/
 
 sudo gem install fpm
