af798b1b8d14941711295d43ed5595b388e4264f
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-07-11 11:38:51 -0600

    Update create_istio_release.sh

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -53,6 +53,11 @@ if [[ ${TAG} =~ "fips" ]]; then
     export ISTIO_ENVOY_BASE_URL=https://storage.googleapis.com/getistio-build/proxy-fips
 fi
 
+
+if [[ "$(uname -m)" = "x86_64" ]]; then
+    export BASE_VERSION=1.13-2022-05-26T19-03-02 
+fi
+
 if [[ "$(uname -m)" = "aarch64" ]]; then
     sed -i 's/gcr\.io\/istio-release/gcr\.io\/tetrate-istio-arm/' $(find ${BASEDIR} | grep Dockerfile)
     sed -i 's/gcr\.io\/tetrate-istio-arm\/iptables@sha256:[0-9a-f]*/gcr\.io\/istio-release\/iptables@sha256:8efeb55ddf08f2f513d303b8f0ff42c9f08f355de2f4124e641d209d11a6af91/' ${BASEDIR}/pilot/docker/Dockerfile.proxyv2
@@ -60,6 +65,9 @@ if [[ "$(uname -m)" = "aarch64" ]]; then
     export BASE_VERSION=1602e34d9524a2a312907aab276bcd7100da52df # 1.12
     
 fi
+
+
+
 
 # HACK : default manifest from release builder is modified
 echo "Generating the manifests"
