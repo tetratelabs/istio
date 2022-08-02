59632ffdad149e5cbe5d1b24da6e181636e7762e
psbrar99 <brargg1989@gmail.com>
2022-08-02 13:23:20 -0600

    1.14.3 e2etests

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -23,7 +23,7 @@ if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
-    export GOLANG_VERSION=1.18.2
+    export GOLANG_VERSION=1.18.4
 fi
 
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
