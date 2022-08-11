c6c9ee1dee399624bc1490fba3cbe24fb0956eba
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-08-10 22:21:27 -0600

    Update setup_go.sh

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -3,27 +3,27 @@ set -e
 set -u
 
 if $(grep -q "1.7" <<< $TAG); then
-    export GOLANG_VERSION=1.14.12
+  export GOLANG_VERSION=1.14.12b4
 fi
 
 if $(grep -q "1.8" <<< $TAG || grep -q "1.9" <<< $TAG); then
-    export GOLANG_VERSION=1.15.7
+  export GOLANG_VERSION=1.15.8
 fi
 
 if $(grep -q "1.10" <<< $TAG); then
-    export GOLANG_VERSION=1.16.9
+  export GOLANG_VERSION=1.16.9
 fi
 
 if $(grep -q "1.11" <<< $TAG); then
-    export GOLANG_VERSION=1.17.6
+  export GOLANG_VERSION=1.17.6
 fi
 
-if $(grep -q "1.12" <<< $TAG) 
-    export GOLANG_VERSION=1.17.8
+if $(grep -q "1.12" <<< $TAG); then
+  export GOLANG_VERSION=1.17.8
 fi
 
 if $(grep -q "1.13" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
-    export GOLANG_VERSION=1.18.4
+  export GOLANG_VERSION=1.18.4
 fi
 
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
