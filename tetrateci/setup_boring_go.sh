3cc4b923864d31dfe77139a865fb7b193cd74d0f
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-08-10 21:54:23 -0600

    Update setup_boring_go.sh

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -18,12 +18,12 @@ if $(grep -q "1.11" <<< $TAG); then
   export GOLANG_VERSION=1.17.6b7
 fi
 
-if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
+if $(grep -q "1.12" <<< $TAG); then
   export GOLANG_VERSION=1.17.8b7
 fi
 
-if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
-    export GOLANG_VERSION=1.18.4b7
+if $(grep -q "1.13" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
+  export GOLANG_VERSION=1.18.4b7
 fi
 
 url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
