316318a65638d0f5526172cdadf384f6ad06eb86
psbrar99 <brargg1989@gmail.com>
2022-09-27 11:48:22 -0600

    1.15.1 fips build

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -22,7 +22,15 @@ if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "
     export GOLANG_VERSION=1.17.8
 fi
 
-url="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
+if $(grep -q "1.14" <<< $TAG); then
+    export GOLANG_VERSION=1.18.4
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.15" ]]; then
+    export GOLANG_VERSION=1.19
+fi
+
+url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
 
 wget -q -O go.tgz "$url"
 
