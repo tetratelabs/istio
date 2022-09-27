aff160026e3bc3879807753d81407532f8d0d14c
psbrar99 <brargg1989@gmail.com>
2022-09-27 12:10:59 -0600

    bumping go for 1.15.1

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -26,6 +26,10 @@ if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
     export GOLANG_VERSION=1.18.2b7
 fi
 
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.15" ]]; then
+    export GOLANG_VERSION=1.19.1b7
+fi
+
 url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
 
 wget -q -O go.tgz "$url"
