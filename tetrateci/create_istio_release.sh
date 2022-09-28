dcf68ba6736f4aec59557fe4925b8583da047cb0
Prabhjot Singh <86856448+psbrar99@users.noreply.github.com>
2022-09-28 07:14:06 -0600

    Update create_istio_release.sh

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -166,6 +166,7 @@ if [[ -z ${TEST:-} ]]; then
     done
 fi
 echo "Cleaning /tmp/istio...."
-[ -d "/tmp/istio-release" ] && sudo rm -rf /tmp/istio-release
+
+#[ -d "/tmp/istio-release" ] && sudo rm -rf /tmp/istio-release
 
 echo "Done building and pushing the artifacts."
