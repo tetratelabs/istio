177f7481dc313aa7e8cf23872c65d643b173ac8c
psbrar99 <brargg1989@gmail.com>
2022-10-04 23:09:35 -0600

    Istio 1.15.1 tetratefips exp

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -30,7 +30,7 @@ if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.15" ]]; then
     export GOLANG_VERSION=1.19.1
 fi
 
-url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
+url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
 
 wget -q -O go.tgz "$url"
 
