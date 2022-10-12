5f54f3b7d14606f2f1336e7b32e99354e89bcd95
psbrar99 <brargg1989@gmail.com>
2022-10-11 22:02:31 -0600

    trivy testing

--- tetrateci/images.sh
+++ tetrateci/images.sh
@@ -6,25 +6,20 @@ set -o pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
+mkdir getistiotesting
+
 IMAGES=(app
-app_sidecar_centos_7
-app_sidecar_centos_8
-app_sidecar_debian_10
-app_sidecar_debian_9
-app_sidecar_ubuntu_bionic
-app_sidecar_ubuntu_focal
-app_sidecar_ubuntu_xenial
 install-cni
 istioctl
-operator
 pilot
 proxyv2)
+
 
 IMAGE_SUFFIXES=("debug" "distroless")
 
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
-    echo $HUB/${image}:${TAG}-${suffix} >> list.txt
+    echo getistiotesting/${image}:${TAG}-${suffix} >> list.txt
     cat list.txt
   done
 done
