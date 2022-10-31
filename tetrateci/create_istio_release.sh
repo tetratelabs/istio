6582855202afbe8f8685912253ec2b6617aba461
psbrar99 <brargg1989@gmail.com>
2022-10-30 23:32:10 -0600

    1.15.3 tetratefips release

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -112,7 +112,7 @@ fi
 sudo apt-get install rpm -y
 # Build Docker Images
 sudo rm -rf /tmp/istio-release && mkdir /tmp/istio-release
-go run main.go build --manifest manifest.docker.yaml
+GOEXPERIMENT=boringcrypto go run main.go build --manifest manifest.docker.yaml
 # go run main.go validate --release /tmp/istio-release/out # seems like it fails if not all the targets are generated
 
 #loading pilot image manually since docker container create command is failing due to unavailbilty of pilot image locally
