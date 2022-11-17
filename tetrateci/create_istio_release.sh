81024619493563fb72ad45189655d2e3e1d17d5e
psbrar99 <brargg1989@gmail.com>
2022-11-16 18:10:24 -0700

    1.14.5 multiarch

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -99,6 +99,10 @@ if [[ ${TAG} =~ "fips" ]]; then
   sed -i 's/export CGO_ENABLED=${CGO_ENABLED:-0}/'"$text"'/g' istio/common/scripts/gobuild.sh
 fi
 
+if [[ "$(uname -m)" = "x86_64" ]]; then
+    export BASE_VERSION=1.13-2022-05-26T19-03-02 
+fi
+
 # Generalizing TAG variable exporting option to incorporate ARM build.We need amd64 and arm64 suffix in docker images to create multi-arch images.Not needed for tetrate and tetratefips build.
 if [[ ${TAG} =~ "multiarch" ]]; then
   if  [[ "$(uname -m)" = "aarch64" ]]; then
