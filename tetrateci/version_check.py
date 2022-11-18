2fff45acbcedbe5564ba23d929fac6fb50923246
psbrar99 <brargg1989@gmail.com>
2022-11-18 12:10:03 -0700

    fixed version for supported k8s version

--- tetrateci/version_check.py
+++ tetrateci/version_check.py
@@ -12,6 +12,7 @@ version_matrix = {
     "1.13": {"1.20", "1.21", "1.22", "1.23"}, # officially supported versions according to https://istio.io/latest/news/releases/1.13.x/announcing-1.13
     "1.14": {"1.20","1.21", "1.22", "1.23", "1.24"} , # officially supported versions according to https://istio.io/latest/news/releases/1.14.x/announcing-1.14
     "1.15": {"1.21","1.22", "1.23", "1.24", "1.25"} , # officially supported versions according to https://istio.io/latest/news/releases/1.15.x/announcing-1.15
+    "1.15": {"1.22", "1.23", "1.24", "1.25"} , # officially supported versions according to https://istio.io/latest/news/releases/1.16.x/announcing-1.16
 }
 
 istio_ver = os.environ.get("ISTIO_MINOR_VER")
