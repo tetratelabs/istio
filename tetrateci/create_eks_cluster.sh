8d3695d71a46cc4447306a80b173f0f6cb66be68
psbrar99 <brargg1989@gmail.com>
2023-02-15 16:29:37 -0700

    adding eks tags

--- tetrateci/create_eks_cluster.sh
+++ tetrateci/create_eks_cluster.sh
@@ -32,4 +32,4 @@ if [[ "${TEST_ARCH}" = "arm64" ]]; then
 fi
 
 echo "creating a eks cluster with \"$CLUSTER_NAME\" name..."
-eksctl create cluster --name $CLUSTER_NAME --version $K8S_VERSION --nodes 3 --node-type $NODE_TYPE
+eksctl create cluster --name $CLUSTER_NAME --version $K8S_VERSION --nodes 3 --node-type $NODE_TYPE --tags tetrate:owner=psb,tetrate:purpose=development,tetrate:team=eng:platform,tetrate:customer=internal,tetrate:lifespan=ongoing
