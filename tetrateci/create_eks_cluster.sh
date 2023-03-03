bd26a5b9ce8e37de01d1ef5797381a276fb29cdf
psbrar99 <brargg1989@gmail.com>
2023-03-03 05:15:49 -0700

    1.14.6 tetratefips rebuild for informatica

--- tetrateci/create_eks_cluster.sh
+++ tetrateci/create_eks_cluster.sh
@@ -26,7 +26,7 @@ fi
 SHA8=$(git rev-parse --short $GITHUB_SHA)
 SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION.$TEST_ARCH)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
-NODE_TYPE="m5.xlarge"
+NODE_TYPE="r5.2xlarge"
 if [[ "${TEST_ARCH}" = "arm64" ]]; then
     NODE_TYPE="m6g.xlarge"
 fi
