4c7be9f724d683a4cbc91016c71bf2c3344ec9b9
psbrar99 <brargg1989@gmail.com>
2023-04-06 11:37:35 -0600

    1.15.7 tetratefips release

--- pkg/test/framework/components/istio/util.go
+++ pkg/test/framework/components/istio/util.go
@@ -132,6 +132,15 @@ func getRemoteServiceAddress(s *kube.Settings, cluster cluster.Cluster, ns, labe
 	if ingr.IP == "" && ingr.Hostname == "" {
 		return nil, false, fmt.Errorf("service %s/%s is not available yet: no ingress", svc.Namespace, svc.Name)
 	}
+	if ingr.Hostname != "" {
+		ip, err := net.LookupIP(ingr.Hostname)
+		if err != nil {
+			return nil, false, fmt.Errorf("service %s/%s is not available yet: no ingress", svc.Namespace, svc.Name)
+		}
+		if len(ip) > 0 {
+			ingr.IP = ip[0].String()
+		}
+	}
 	if ingr.IP != "" {
 		return net.TCPAddr{IP: net.ParseIP(ingr.IP), Port: port}, true, nil
 	}
