e3917bc590d43813abfbaacc8ecea51611addc80
psbrar99 <brargg1989@gmail.com>
2023-04-27 22:10:37 -0600

    testing

--- tetrateci/gen_release_manifest.py
+++ tetrateci/gen_release_manifest.py
@@ -26,6 +26,7 @@ with open(source_yaml, "r") as file :
     manifest["docker"] = hub
     manifest["version"] = tag
     manifest["dependencies"]["istio"] = {"localpath" : "./istio"}
+    manifest["dependencies"]["proxy"]["git"] = "https://github.com/envoyproxy/envoy"
     manifest["dependencies"]["client-go"]["branch"] = branch
     manifest["dependencies"]["tools"]["branch"] = branch
     # genproto has been removed from 1.14
