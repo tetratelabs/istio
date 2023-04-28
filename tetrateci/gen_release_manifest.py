2756378fd607bd3d77148c380f0a3225f9e983c6
psbrar99 <brargg1989@gmail.com>
2023-04-27 22:38:11 -0600

    testing

--- tetrateci/gen_release_manifest.py
+++ tetrateci/gen_release_manifest.py
@@ -34,7 +34,8 @@ with open(source_yaml, "r") as file :
     # added check for "gogo-genproto" dependenciy if it present then assign branch
     if "gogo-genproto" in manifest["dependencies"]:
         manifest["dependencies"]["gogo-genproto"]["branch"] = branch
-    manifest["dependencies"]["envoy"]["git"] = "https://github.com/istio/envoy"
+    manifest["dependencies"]["envoy"]["git"] = "https://github.com/envoyproxy/envoy"
+    manifest["dependencies"]["envoy"]["sha"] = "e0433be316e635e7b032079aadc5efcf4fcada25"
     manifest['outputs'] = ["docker"]
     f = open(os.path.join(destination_folder, "manifest.docker.yaml"), 'w')
     yaml.dump(manifest, f)
