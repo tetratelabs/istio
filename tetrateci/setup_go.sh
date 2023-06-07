25677f72f1717671a9aea06a4b890d1cb9851e1f
psbrar99 <brargg1989@gmail.com>
2023-06-06 19:21:35 -0600

    fix cve

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -31,7 +31,7 @@ if $(grep -q "1.15" <<< $TAG); then
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.16" ]]; then
-    export GOLANG_VERSION=1.19.7
+    export GOLANG_VERSION=1.19.10
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
