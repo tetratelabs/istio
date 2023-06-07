8dc595bd48895edbb45cf9e630bced413c83c212
psbrar99 <brargg1989@gmail.com>
2023-06-07 17:36:20 -0600

    tetratefips 1.18.0 release

---
+++ tetrateci/create_eks_cluster.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+./tetrateci/version_check.py && exit
+
+set -o errexit
+set -o pipefail
+
+if [[ ! -f ~/.aws/config && ! -f ~/.aws/credentials ]]
+then
+    echo "warn: didn't find config and credentials in ~/.aws."
+    echo "checking for environment varibles...."
+    if [[ ! -v AWS_ACCESS_KEY_ID && ! -v AWS_SECRET_ACCESS_KEY ]]
+    then
+        echo "error: neither is aws_access_key_id and aws_secret_access_key is set."
+        exit 2
+    fi
+fi
+
+if ! command -v eksctl &> /dev/null
+then
+    echo "warn: eksctl is not found in the \$path. downloading eksctl"
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+    sudo mv /tmp/eksctl /usr/local/bin
+fi
+
+SHA8=$(git rev-parse --short $GITHUB_SHA)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION.$TEST_ARCH)
+CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
+NODE_TYPE="r5.2xlarge"
+if [[ "${TEST_ARCH}" = "arm64" ]]; then
+    NODE_TYPE="m6g.2xlarge"
+fi
+
+echo "creating a eks cluster with \"$CLUSTER_NAME\" name..."
+eksctl create cluster --name $CLUSTER_NAME --version $K8S_VERSION --nodes 3 --node-type $NODE_TYPE --tags tetrate:owner=psb,tetrate:purpose=development,tetrate:team=eng:tid,tetrate:customer=internal,tetrate:lifespan=ongoing
+
---
+++ tetrateci/setup_go.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+if $(grep -q "1.7" <<< $TAG); then
+    export GOLANG_VERSION=1.14.12
+fi
+
+if $(grep -q "1.8" <<< $TAG || grep -q "1.9" <<< $TAG); then
+    export GOLANG_VERSION=1.15.7
+fi
+
+if $(grep -q "1.10" <<< $TAG); then
+    export GOLANG_VERSION=1.16.9
+fi
+
+if $(grep -q "1.11" <<< $TAG); then
+    export GOLANG_VERSION=1.17.6
+fi
+
+if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.13" ]]; then
+    export GOLANG_VERSION=1.17.8
+fi
+
+if $(grep -q "1.14" <<< $TAG); then
+    export GOLANG_VERSION=1.18.4
+fi
+
+if $(grep -q "1.15" <<< $TAG); then
+    export GOLANG_VERSION=1.19.2
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.16" ]]; then
+    export GOLANG_VERSION=1.19.5
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
+    export GOLANG_VERSION=1.20.2
+fi
+
+
+url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
+
+wget -q -O go.tgz "$url"
+
+sudo tar -C /usr/local -xzf go.tgz
+rm go.tgz
+
+export GOROOT=/usr/local/go
+export PATH="$GOROOT/bin:$PATH"
+
+echo "Go installed"
+go version
