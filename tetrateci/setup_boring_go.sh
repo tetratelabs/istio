d4d9d244c4f4aa459811be10bde180d7cdf718aa
psbrar99 <brargg1989@gmail.com>
2023-06-08 08:56:50 -0600

    bump golang

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -38,6 +38,10 @@ if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
     export GOLANG_VERSION=1.20.1
 fi
 
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.18" ]]; then
+    export GOLANG_VERSION=1.20.5
+fi
+
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
 
 wget -q -O go.tgz "$url"
