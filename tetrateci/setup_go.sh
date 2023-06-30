479e9bafebe84162a52d9a5dda38333ffeda490b
psbrar99 <brargg1989@gmail.com>
2023-06-29 18:23:34 -0600

    Bump go

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -31,11 +31,11 @@ if $(grep -q "1.15" <<< $TAG); then
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.16" ]]; then
-    export GOLANG_VERSION=1.19.7
+    export GOLANG_VERSION=1.20.5
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
-    export GOLANG_VERSION=1.20.2
+    export GOLANG_VERSION=1.20.5
 fi
 
 
