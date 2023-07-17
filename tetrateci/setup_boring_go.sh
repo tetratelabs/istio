2e5685bb33fd183ac92391e4af6b047a81ce1f1d
psbrar99 <brargg1989@gmail.com>
2023-07-17 09:44:34 -0600

    tetratefips 1.17.4
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -35,7 +35,7 @@ if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.15" ]]; then
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
-    export GOLANG_VERSION=1.20.1
+    export GOLANG_VERSION=1.20.6
 fi
 
 url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
