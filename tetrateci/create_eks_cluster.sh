cb416db2b0444f8cdfecffb43c43a205ed3d48fd
psbrar99 <brargg1989@gmail.com>
2023-07-25 14:23:38 -0600

    1.17.5 tetrate build
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_eks_cluster.sh
+++ tetrateci/create_eks_cluster.sh
@@ -24,8 +24,13 @@ then
 fi
 
 SHA8=$(git rev-parse --short $GITHUB_SHA)
-SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION.$TEST_ARCH)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
+NODE_TYPE="r5.2xlarge"
+if [[ "${TEST_ARCH}" = "arm64" ]]; then
+    NODE_TYPE="m6g.2xlarge"
+fi
 
 echo "creating a eks cluster with \"$CLUSTER_NAME\" name..."
-eksctl create cluster --name $CLUSTER_NAME --version $K8S_VERSION --nodes 3 --node-type m5.xlarge
+eksctl create cluster --name $CLUSTER_NAME --version $K8S_VERSION --nodes 3 --node-type $NODE_TYPE --tags tetrate:owner=psb,tetrate:purpose=development,tetrate:team=eng:tid,tetrate:customer=internal,tetrate:lifespan=ongoing
+
--- tetrateci/setup_boring_go.sh
+++ tetrateci/setup_boring_go.sh
@@ -22,7 +22,23 @@ if $(grep -q "1.12" <<< $TAG) || [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "
   export GOLANG_VERSION=1.17.8b7
 fi
 
-url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
+    export GOLANG_VERSION=1.18.2b7
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.14" ]]; then
+    export GOLANG_VERSION=1.18.2b7
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.15" ]]; then
+    export GOLANG_VERSION=1.19.2
+fi
+
+if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
+    export GOLANG_VERSION=1.20.6
+fi
+
+url="https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz"
 
 wget -q -O go.tgz "$url"
 
