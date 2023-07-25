f279928fc72b5caa409ce428393cf3754df89552
psbrar99 <brargg1989@gmail.com>
2023-07-25 14:33:50 -0600

    bump go
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/setup_go.sh
+++ tetrateci/setup_go.sh
@@ -35,7 +35,7 @@ if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.16" ]]; then
 fi
 
 if [[ "${REL_BRANCH_VER:-${ISTIO_MINOR_VER}}" == "1.17" ]]; then
-    export GOLANG_VERSION=1.20.7
+    export GOLANG_VERSION=1.20.6
 fi
 
 
