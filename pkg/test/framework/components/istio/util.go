ffe3d21415c1ab551c7ffd229248f8fe95d4a28e
psbrar99 <brargg1989@gmail.com>
2023-07-27 13:11:47 -0600

    patch eks lb
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- pkg/test/framework/components/istio/util.go
+++ pkg/test/framework/components/istio/util.go
@@ -137,6 +137,15 @@ func getRemoteServiceAddress(s *kube.Settings, cluster cluster.Cluster, ns, labe
 	if ingr.IP == "" && ingr.Hostname == "" {
 		return nil, false, fmt.Errorf("service %s/%s is not available yet: no ingress", svc.Namespace, svc.Name)
 	}
+	if ingr.Hostname != "" {
+		ip, err := net.LookupIP(ingr.Hostname)
+		if err != nil {
+			return nil, false, fmt.Errorf("service %s/%s is not available yet: no ingress", svc.Namespace, svc.Name)
+		}
+		if len(ip) > 0 {
+			ingr.IP = ip[0].String()
+		}
+	}
 	if ingr.IP != "" {
 		ipaddr, err := netip.ParseAddr(ingr.IP)
 		if err != nil {
