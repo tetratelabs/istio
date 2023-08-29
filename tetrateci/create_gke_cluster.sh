8b78b3ef6a510c366342e0948c6ce122ab41aa19
psbrar99 <brargg1989@gmail.com>
2023-08-29 16:14:50 -0600

    fix gke testing
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_gke_cluster.sh
+++ tetrateci/create_gke_cluster.sh
@@ -8,5 +8,7 @@ set -o pipefail
 SHA8=$(git rev-parse --short $GITHUB_SHA)
 SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+gcloud components install gke-gcloud-auth-plugin
 gcloud container clusters create $CLUSTER_NAME --machine-type "n1-standard-4" --num-nodes 3 --region=us-central1-c --enable-network-policy --cluster-version $K8S_VERSION --release-channel "$CHAN"
 gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1-c
