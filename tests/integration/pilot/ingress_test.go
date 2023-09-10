81c4fd50e3566e5c41330ee475cec7e097024ec3
psbrar99 <brargg1989@gmail.com>
2023-09-10 13:12:02 -0600

    1.19.0 tetrate release
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tests/integration/pilot/ingress_test.go
+++ tests/integration/pilot/ingress_test.go
@@ -332,6 +332,12 @@ spec:
 					if hostIsIP {
 						got = ing.Status.LoadBalancer.Ingress[0].IP
 					}
+					if ing.Status.LoadBalancer.Ingress[0].Hostname != "" {
+						ip, _ := net.LookupIP(ing.Status.LoadBalancer.Ingress[0].Hostname)
+						if len(ip) > 0 {
+							got = ip[0].String()
+						}
+					}
 					if got != host {
 						return fmt.Errorf("unexpected ingress status, got %+v want %v", got, host)
 					}
