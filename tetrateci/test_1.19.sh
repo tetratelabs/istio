8fec3695b8eb662067a4509b16874e2316f28d75
psbrar99 <brargg1989@gmail.com>
2023-09-11 16:45:36 -0600

    debugging e2e tests
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/test_1.19.sh
+++ tetrateci/test_1.19.sh
@@ -72,20 +72,7 @@ for pkg in $PACKAGES; do
     --istio.test.ci \
     --istio.test.skipVM=true \
     --istio.test.hub=${HUB} \
-    --istio.test.tag=${TAG}-distroless \
-    --istio.test.pullpolicy=IfNotPresent \
-    --istio.test.retries=1 \
-    ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
-    && \
-  go test \
-    -test.v \
-    -timeout 2h \
-    -tags=integ \
-    "${pkg}" \
-    --istio.test.select=-postsubmit,-flaky \
-    ${SKIP_TEST_FLAGS[@]+"${SKIP_TEST_FLAGS[@]}"} \
-    --istio.test.ci \
-    --istio.test.skipVM=true \
+    --istio.test.tag=${TAG} \
     --istio.test.pullpolicy=IfNotPresent \
     --istio.test.retries=1 \
     ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
