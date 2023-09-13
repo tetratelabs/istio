a261cc7dae88fc9a0332b8a1d4cc107f05ac9abd
psbrar99 <brargg1989@gmail.com>
2023-09-13 17:00:38 -0600

    bump base images and run e2e tests
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -46,6 +46,7 @@ if [[ ${TAG} =~ "fips" ]]; then
 	# Escape '/'
 	PROXY_DISTROLESS_BASE_ESCAPED=$(sed 's/\//\\\//g' <<< ${PROXY_DISTROLESS_BASE})
 	sed -i "s/.*as distroless/${PROXY_DISTROLESS_BASE_ESCAPED}/" ${BASEDIR}/operator/docker/Dockerfile.operator
+        sed -i "s/.*as distroless/${PROXY_DISTROLESS_BASE_ESCAPED}/" ${BASEDIR}/pilot/docker/Dockerfile.pilot
         export ISTIO_ENVOY_BASE_URL=https://storage.googleapis.com/getistio-build/proxy-fips
 fi
 
