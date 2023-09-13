b6eac5725c17f9275d792ba1a943feee9a24d723
psbrar99 <brargg1989@gmail.com>
2023-09-12 21:04:40 -0600

    eks patch
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/test_1.19_arm.sh
+++ tetrateci/test_1.19_arm.sh
@@ -78,19 +78,6 @@ for pkg in $PACKAGES; do
     --istio.test.pullpolicy=IfNotPresent \
     --istio.test.retries=1 \
     ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
-    && \
-  go test \
-    -test.v \
-    -timeout 2h \
-    -tags=integ \
-    "${pkg}" \
-    --istio.test.select=-postsubmit,-flaky \
-    ${SKIP_TEST_FLAGS[@]+"${SKIP_TEST_FLAGS[@]}"} \
-    --istio.test.ci \
-    --istio.test.skipVM=true \
-    --istio.test.pullpolicy=IfNotPresent \
-    --istio.test.retries=1 \
-    ${COMMON_TEST_FLAGS[@]+"${COMMON_TEST_FLAGS[@]}"} \
     || \
     { FAILED_PACKAGES+=( "${pkg}" ) && echo "Test Failed: ${pkg}" ; }
 
