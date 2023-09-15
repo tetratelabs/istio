f125a9fd70238a2ada147bd6319307e570003e0d
psbrar99 <brargg1989@gmail.com>
2023-09-15 09:18:59 -0600

    debug
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -207,7 +207,7 @@ ifeq ($(DEBUG),1)
 # gobuild script uses custom linker flag to set the variables.
 RELEASE_LDFLAGS=''
 else
-RELEASE_LDFLAGS='-extldflags -static -s -w'
+RELEASE_LDFLAGS='-linkmode 'external' -extldflags -static -s -w'
 endif
 
 # List of all binaries to build
