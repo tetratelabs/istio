e900a075564abc7ff2b836f430332d097c12815f
psbrar99 <brargg1989@gmail.com>
2023-10-10 10:23:39 -0600

    1.16.7 tetratefips build
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

---
+++ tetrateci/push_ecr.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+# set -x
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+
+IMAGES=(install-cni
+istioctl
+operator
+pilot
+proxyv2)
+
+IMAGE_SUFFIXES=("" "-debug" "-distroless")
+
+for image in "${IMAGES[@]}"; do
+  for suffix in "${IMAGE_SUFFIXES[@]}"; do
+    docker tag $HUB/${image}:${TAG}${suffix} $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+    echo $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+    docker push $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+  done
+done
