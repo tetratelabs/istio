44e094b0e8f1eacca96459d3ea9411b0ecd3a4d3
psbrar99 <brargg1989@gmail.com>
2023-10-12 22:31:28 -0600

    Publish release
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -152,20 +152,20 @@ fi
 
 # If RELEASE, Build Archives
 if [[ -z ${TEST:-} ]]; then
-    IMAGES=(install-cni
-    proxyv2
-    operator
-    istioctl
-    pilot)
+    # IMAGES=(install-cni
+    # proxyv2
+    # operator
+    # istioctl
+    # pilot)
 
-    IMAGE_SUFFIXES=("" "-debug" "-distroless")
+    # IMAGE_SUFFIXES=("" "-debug" "-distroless")
 
-    for image in "${IMAGES[@]}"; do
-      for suffix in "${IMAGE_SUFFIXES[@]}"; do
-        DIGEST=$(crane digest $HUB/${image}:${TAG}${suffix})
-        cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$DIGEST
-      done
-    done
+    # for image in "${IMAGES[@]}"; do
+    #   for suffix in "${IMAGE_SUFFIXES[@]}"; do
+    #     DIGEST=$(crane digest $HUB/${image}:${TAG}${suffix})
+    #     cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$DIGEST
+    #   done
+    # done
 
     
 
