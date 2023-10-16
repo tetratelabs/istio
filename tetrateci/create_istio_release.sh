56cf80ea2260dcf2cdafcafd0b058d3ce3167353
psbrar99 <brargg1989@gmail.com>
2023-10-16 15:13:50 -0600

    publish artifacts
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -135,7 +135,7 @@ if [ ${TAG} =~ "fips" ]; then
     [[ $CHECK_CRYPTO == X:boringcrypto ]] || exit 1
 fi
 
-# go run main.go publish --release /tmp/istio-release/out --dockerhub $HUB
+go run main.go publish --release /tmp/istio-release/out --dockerhub $HUB
 
 
 
