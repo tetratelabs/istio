bb3152cb3c40ed105fc4c9e052203e67274b2807
psbrar99 <brargg1989@gmail.com>
2023-10-16 21:04:36 -0600

    fix cosigning
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/images.sh
+++ tetrateci/images.sh
@@ -18,7 +18,7 @@ IMAGE_SUFFIXES=("debug" "distroless")
 
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
-    DIGEST=$(crane digest $HUB/${image}:${TAG}${suffix})
+    DIGEST=$(crane digest $HUB/${image}:${TAG}-${suffix})
     cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$DIGEST
   done
 done
