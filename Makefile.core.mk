65a873d737fb27842c5557820b8f742a607f08fd
psbrar99 <brargg1989@gmail.com>
2023-10-18 22:57:19 -0600

    bump base image
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -38,7 +38,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.16-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2023-10-12T19-01-47
+BASE_VERSION ?= 1.18-2023-10-12T19-01-45
 
 export GO111MODULE ?= on
 export GOPROXY ?= https://proxy.golang.org
