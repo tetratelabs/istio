6669d452f46e105676f6c961f9b83a8a1cb6bc69
psbrar99 <brargg1989@gmail.com>
2023-11-16 12:06:32 -0700

    publish 1.18.5-tetratefips-v3
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/push_ecr.sh
+++ tetrateci/push_ecr.sh
@@ -24,9 +24,10 @@ for image in "${IMAGES[@]}"; do
   done
 done
 
+# do not copy if it is a FIPS build or backport.
 if  [[ ${TAG} =~ "fips" ]] ;then
   exit 0;
-elif [[ ${BACKPORT} == "false" ]] ; then
+elif [[ ${BACKPORT} == "false" ]]  ; then
     for image in "${IMAGES[@]}"; do
         for suffix in "${IMAGE_SUFFIXES[@]}"; do
             docker tag $HUB/${image}:${TAG}${suffix} $PUBLIC_HUB/${image}:${TAG}${suffix}
@@ -34,6 +35,4 @@ elif [[ ${BACKPORT} == "false" ]] ; then
             docker push $PUBLIC_HUB/${image}:${TAG}${suffix}
         done
     done
-else
-  echo "Images synced"
 fi
