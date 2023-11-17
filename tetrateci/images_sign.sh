becd9c13d2394dc5fc11095c0db101fef7a92e4c
psbrar99 <brargg1989@gmail.com>
2023-11-17 13:35:44 -0700

    test
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/images_sign.sh
+++ tetrateci/images_sign.sh
@@ -27,17 +27,17 @@ TG1=(1.19.3-tetrate-v1
 1.18.5-tetrate-v2
 1.18.5-tetrate-v3)
 
-HB=fips-containers.istio.tetratelabs.com
+# HB=fips-containers.istio.tetratelabs.com
 HB1=containers.istio.tetratelabs.com
 
 IMAGE_SUFFIXES=("debug" "distroless")
 
-for image in "${IMAGES[@]}"; do
-  for suffix in "${IMAGE_SUFFIXES[@]}"; do
-    DIGEST=$(crane digest $HB/${image}:${TG}-${suffix})
-    cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HB/${image}@$DIGEST
-  done
-done
+# for image in "${IMAGES[@]}"; do
+#   for suffix in "${IMAGE_SUFFIXES[@]}"; do
+#     DIGEST=$(crane digest $HB/${image}:${TG}-${suffix})
+#     cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HB/${image}@$DIGEST
+#   done
+# done
 
 
 
