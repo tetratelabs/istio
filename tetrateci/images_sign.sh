7d7b29cce3e6fa728776021a557426c5c08092c6
psbrar99 <brargg1989@gmail.com>
2023-11-17 16:29:15 -0700

    test
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

---
+++ tetrateci/images_sign.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+# set -x
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+
+
+# IMAGES=(install-cni
+# proxyv2
+# operator
+# istioctl
+# pilot)
+
+IMAGES=(ztunnel)
+
+
+
+
+TG1=(1.19.3-tetratefips-v1
+1.18.5-tetratefips-v2
+1.18.5-tetratefips-v3)
+
+
+
+
+
+IMAGE_SUFFIXES=("debug" "distroless")
+
+
+for image in "${IMAGES[@]}"; do
+    for suffix in "${IMAGE_SUFFIXES[@]}"; do
+        for tag in "${TG1[@]}"; do 
+            docker pull $HUB/${image}:${tag}-${suffix}
+            DIGEST=$(crane digest $HUB/${image}:${tag}-${suffix})
+            echo "Signing $HUB/${image}:${tag}-${suffix}"
+            cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$DIGEST
+        done    
+    done
+done
