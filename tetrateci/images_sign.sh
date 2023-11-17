d5656c38e6a5d6e5cdcb9bced5cc7d4545540aa8
psbrar99 <brargg1989@gmail.com>
2023-11-17 13:49:37 -0700

    test
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/images_sign.sh
+++ tetrateci/images_sign.sh
@@ -14,21 +14,23 @@ operator
 istioctl
 pilot)
 
-TG=(1.19.3-tetrate-v1
-1.19.4-tetrate-v0
-1.17.8-tetratefips-v1
-1.18.5-tetrate-v2
-1.18.5-tetrate-v3
-1.16.7-tetratefips-v3)
+# TG=(1.19.3-tetrate-v1
+# 1.19.4-tetrate-v0
+# 1.17.8-tetratefips-v1
+# 1.18.5-tetrate-v2
+# 1.18.5-tetrate-v3
+# 1.16.7-tetratefips-v3)
 
 TG1=(1.19.3-tetrate-v1
 1.19.4-tetrate-v0
-1.17.8-tetratefips-v1
+1.17.8-tetrate-v1
 1.18.5-tetrate-v2
 1.18.5-tetrate-v3)
 
+
+
 # HB=fips-containers.istio.tetratelabs.com
-HB1=containers.istio.tetratelabs.com
+
 
 IMAGE_SUFFIXES=("debug" "distroless")
 
@@ -40,10 +42,11 @@ IMAGE_SUFFIXES=("debug" "distroless")
 # done
 
 
-
+HB1=containers.istio.tetratelabs.com
 for image in "${IMAGES[@]}"; do
     for suffix in "${IMAGE_SUFFIXES[@]}"; do
         DIGEST=$(crane digest $HB1/${image}:${TG1}-${suffix})
+        echo "Signing $HB1/${image}:${TG1}-${suffix}"
         cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HB1/${image}@$DIGEST
     done
 done
