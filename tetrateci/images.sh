6902a2e69646c24efb8432e2433dc8d3344bdd4c
psbrar99 <brargg1989@gmail.com>
2023-11-20 08:12:10 -0700

    release 1.19.4-tetrate-v0
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/images.sh
+++ tetrateci/images.sh
@@ -20,6 +20,7 @@ IMAGE_SUFFIXES=("debug" "distroless")
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
     DIGEST=$(crane digest $HUB/${image}:${TAG}-${suffix})
+    echo "Signing $HUB/${image}:${TAG}-${suffix}"
     cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $HUB/${image}@$DIGEST
   done
 done
@@ -29,6 +30,7 @@ if  [[ ${TAG} =~ "fips" ]] ;then
 elif [[ ${BACKPORT} == "false" ]] ; then
     for image in "${IMAGES[@]}"; do
         for suffix in "${IMAGE_SUFFIXES[@]}"; do
+            echo "signing $PUBLIC_HUB/${image}:${TAG}-${suffix}"
             DIGEST=$(crane digest $PUBLIC_HUB/${image}:${TAG}-${suffix})
             cosign sign -y --identity-token=$(gcloud auth print-identity-token --audiences=sigstore --include-email --impersonate-service-account image-signing-keyless-sa@tid-testing.iam.gserviceaccount.com) $PUBLIC_HUB/${image}@$DIGEST
         done
