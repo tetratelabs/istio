aba90e56ac8ba62cfc1ac04238dfc3417e4f6d39
psbrar99 <brargg1989@gmail.com>
2023-11-21 11:16:58 -0700

    increate timeout for pilot tests
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -138,8 +138,8 @@ fi
 go run main.go publish --release /tmp/istio-release/out --dockerhub $HUB
 
 
-make -C istio rpm/fpm
-make -C istio deb/fpm
+# make -C istio rpm/fpm
+# make -C istio deb/fpm
 
 
 
