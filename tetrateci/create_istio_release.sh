9052a00b92740eb48480a64aba0e6a76df6602e7
psbrar99 <brargg1989@gmail.com>
2023-11-21 10:54:31 -0700

    increate timeout for pilot tests
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -22,7 +22,7 @@ sudo gem install fpm
 sudo apt-get install go-bindata -y
 export BRANCH=release-${REL_BRANCH_VER}
 cd ..
-rm -rf release-builder
+sudo rm -rf release-builder
 git clone https://github.com/istio/release-builder --branch ${BRANCH}
 
 
