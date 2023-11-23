14f4ddf32fb2f77c3eb2a72ed1b1dbf13e59a065
psbrar99 <brargg1989@gmail.com>
2023-11-23 08:37:05 -0700

    1.20.0 tetrate release
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/push_ecr.sh
+++ tetrateci/push_ecr.sh
@@ -18,8 +18,8 @@ IMAGE_SUFFIXES=("" "-debug" "-distroless")
 
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
-    echo ${HUB}/${image}:${tag}${suffix}
-    crane copy ${HUB}/${image}:${tag}${suffix} $ECR_REGISTRY/tid-istio/${image}:${tag}${suffix}
+    echo ${HUB}/${image}:${TAG}${suffix}
+    crane copy ${HUB}/${image}:${TAG}${suffix} $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
 
     # docker tag $HUB/${image}:${TAG}${suffix} $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
     # echo $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
@@ -36,8 +36,8 @@ elif [[ ${BACKPORT} == "false" ]]  ; then
             # docker tag $HUB/${image}:${TAG}${suffix} $PUBLIC_HUB/${image}:${TAG}${suffix}
             # echo $PUBLIC_HUB/${image}:${TAG}${suffix}
             # docker push $PUBLIC_HUB/${image}:${TAG}${suffix}
-            echo ${HUB}/${image}:${tag}${suffix}
-            crane copy ${HUB}/${image}:${tag}${suffix} $PUBLIC_HUB/${image}:${tag}${suffix}
+            echo ${HUB}/${image}:${TAG}${suffix}
+            crane copy ${HUB}/${image}:${TAG}${suffix} $PUBLIC_HUB/${image}:${TAG}${suffix}
         done
     done
 fi
