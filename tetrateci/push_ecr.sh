a2b86648765b49225eb7ef7f873e0842ff4944b4
psbrar99 <brargg1989@gmail.com>
2023-11-23 08:31:51 -0700

    1.20.0 tetrate release
    Signed-off-by: psbrar99 <brargg1989@gmail.com>

--- tetrateci/push_ecr.sh
+++ tetrateci/push_ecr.sh
@@ -18,9 +18,12 @@ IMAGE_SUFFIXES=("" "-debug" "-distroless")
 
 for image in "${IMAGES[@]}"; do
   for suffix in "${IMAGE_SUFFIXES[@]}"; do
-    docker tag $HUB/${image}:${TAG}${suffix} $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
-    echo $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
-    docker push $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+    echo ${HUB}/${image}:${tag}${suffix}
+    crane copy ${HUB}/${image}:${tag}${suffix} $ECR_REGISTRY/tid-istio/${image}:${tag}${suffix}
+
+    # docker tag $HUB/${image}:${TAG}${suffix} $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+    # echo $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
+    # docker push $ECR_REGISTRY/tid-istio/${image}:${TAG}${suffix}
   done
 done
 
@@ -30,9 +33,11 @@ if  [[ ${TAG} =~ "fips" ]] ;then
 elif [[ ${BACKPORT} == "false" ]]  ; then
     for image in "${IMAGES[@]}"; do
         for suffix in "${IMAGE_SUFFIXES[@]}"; do
-            docker tag $HUB/${image}:${TAG}${suffix} $PUBLIC_HUB/${image}:${TAG}${suffix}
-            echo $PUBLIC_HUB/${image}:${TAG}${suffix}
-            docker push $PUBLIC_HUB/${image}:${TAG}${suffix}
+            # docker tag $HUB/${image}:${TAG}${suffix} $PUBLIC_HUB/${image}:${TAG}${suffix}
+            # echo $PUBLIC_HUB/${image}:${TAG}${suffix}
+            # docker push $PUBLIC_HUB/${image}:${TAG}${suffix}
+            echo ${HUB}/${image}:${tag}${suffix}
+            crane copy ${HUB}/${image}:${tag}${suffix} $PUBLIC_HUB/${image}:${tag}${suffix}
         done
     done
 fi
